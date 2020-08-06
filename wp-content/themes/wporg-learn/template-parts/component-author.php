bd39d5e684c9d21ce0b46fd922adcda507871a57
dufresnesteven <steve.dufresne@automattic.com>
2020-08-06 13:16:50 +0900

    Rough in the feature component layout.

---
+++ wp-content/themes/wporg-learn/template-parts/component-author.php
@@ -0,0 +1,9 @@
+<div class="workshop-author">
+	<div>
+		<img class="workshop-author_profile" src="https://secure.gravatar.com/avatar/1064bd40c9ade0b3c9cf0e27d9e3432a?s=200&d=mm&r=g" />
+	</div>
+	<div>
+		<div class="workshop-author_name">Jonathan Bossenger</div>
+		<a class="workshop-author_handle" href="">@spykro</a>
+	</div>
+</div>
---
+++ wp-content/themes/wporg-learn/template-parts/component-featured-workshop.php
@@ -0,0 +1,15 @@
+
+<div class="featured-workshop">
+	<div class="featured-workshop_video"></div><!-- This is a placeholder and should be replaced with video -->
+	<div class="featured-workshop_content">
+		<a class="featured-workshop_title" href="">Build your first custom block</a>
+		<div class="row">
+			<div class="col-8">	
+				<p>With WordPress moving more and more into the world of blocks, knowing how to build your own blocks has become valuable knowledge. However, if you are a plugin or theme developer, you might not be sure where to start. This workshop will serve as a guide to building your first block.</p>
+			</div>
+			<div class="col-4 featured-workshop_content_author">
+				<?php get_template_part( 'template-parts/component', 'author' ); ?>
+			</div>
+		</div>
+	</div>
+</div>
