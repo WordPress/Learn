045c06b039ecaa2982acc6eedcb36f80cc901167
dufresnesteven <steve.dufresne@automattic.com>
2020-08-06 11:27:03 +0900

    Add in submit button to heading section.

--- wp-content/themes/wporg-learn/archive-wporg_workshop.php
+++ wp-content/themes/wporg-learn/archive-wporg_workshop.php
@@ -11,7 +11,10 @@ get_header();?>
 <div>
 	<main class="site-main">
 		<section>
-			<h2><?php esc_html_e( 'Workshops', 'wporg-learn' ); ?></h2>
+		<div class="row align-middle between section-heading section-heading--with-button">
+				<h2 class="section-heading_title"><?php esc_html_e( 'Workshops', 'wporg-learn' ); ?></h2>
+				<a class="section-heading_link button button-large" href="/submit-workshop-idea"><?php esc_html_e( 'Submit Workshop Idea', 'wporg-learn' ); ?></a>
+			</div>
 			<hr>
 			<?php get_template_part( 'template-parts/component', 'video-grid' ); ?>
 		</section>
