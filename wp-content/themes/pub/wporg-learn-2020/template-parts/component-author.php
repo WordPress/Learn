12273db9a0d8f1cbf5b94cc18e95374212b6e8ac
dufresnesteven <steve.dufresne@automattic.com>
2020-08-10 12:34:54 +0900

    Add in missing semi colon.

--- wp-content/themes/pub/wporg-learn-2020/template-parts/component-author.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-author.php
@@ -19,7 +19,7 @@ $url = get_avatar_url( get_the_author_meta( 'ID' ) );
 		<img class="workshop-author_profile" src="<?php echo $url; ?>" />
 	</div>
 	<div>
-		<div class="workshop-author_name"><?php echo $display_name ?></div>
+		<div class="workshop-author_name"><?php echo $display_name; ?></div>
 		<?php if( ! empty( $nickname ) ) : ?>
 			<a class="workshop-author_handle" href="<?php echo $user_url; ?>" target="_blank"><?php echo get_the_author_meta( 'nickname' ); ?></a>
 		<?php endif; ?>
