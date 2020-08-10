56684fe14b4de5af7e655b084a3aca74fd55958b
dufresnesteven <steve.dufresne@automattic.com>
2020-08-10 12:00:53 +0900

    Move config to .sh file.

---
+++ bin/index.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Exit if any command fails.
+set -e
+
+# Setup the environment
+npm run wp-env start 
+
+# Update wp configs
+npm run wp-env run cli wp config set JETPACK_DEV_DEBUG true
+npm run wp-env run cli wp config set WPORG_SANDBOXED true
+
+# Activate plugins
+npm run wp-env run cli wp plugin activate wordpress-importer jetpack wporg-learn/wporg-learn.php wporg-markdown/plugin.php
+
+# Activate theme
+npm run wp-env run cli wp theme activate pub/wporg-learn-2020
+
+## Install dependencies
+yarn
+
+## Import lesson plans
+npm run wp-env run cli wp cron event run wporg_learn_manifest_import
+npm run wp-env run cli wp cron event run wporg_learn_markdown_import
+
+## Activate jetpack modules
+npm run wp-env run cli wp jetpack module activate contact-form
+npm run wp-env run cli wp jetpack module activate markdown
+
+## Change permalinks
+npm run wp-env run cli wp rewrite structure '/%postname%/'
+npm run wp-env run cli wp rewrite flush
