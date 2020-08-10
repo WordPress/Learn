a4394fee28eef83a687a2557ba08629f0034b642
dufresnesteven <steve.dufresne@automattic.com>
2020-08-10 12:30:17 +0900

    Use information from the local user for the post.

--- wp-content/themes/pub/wporg-learn-2020/template-parts/component-author.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-author.php
@@ -1,9 +1,27 @@
+<?php
+/**
+ * Template part for display author information
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WPBBP
+ */
+
+$display_name = get_the_author_meta( 'display_name' );
+$nickname = get_the_author_meta( 'nickname' );
+$user_url = get_the_author_meta( 'url' );
+$url = get_avatar_url( get_the_author_meta( 'ID' ) );
+
+?>
+
 <div class="workshop-author">
 	<div>
-		<img class="workshop-author_profile" src="https://secure.gravatar.com/avatar/1064bd40c9ade0b3c9cf0e27d9e3432a?s=200&d=mm&r=g" />
+		<img class="workshop-author_profile" src="<?php echo $url; ?>" />
 	</div>
 	<div>
-		<div class="workshop-author_name"><?php echo get_the_author_meta( 'display_name' ); ?></div>
-		<a class="workshop-author_handle" href="">@spykro</a>
+		<div class="workshop-author_name"><?php echo $display_name ?></div>
+		<?php if( ! empty( $nickname ) ) : ?>
+			<a class="workshop-author_handle" href="<?php echo $user_url; ?>" target="_blank"><?php echo get_the_author_meta( 'nickname' ); ?></a>
+		<?php endif; ?>
 	</div>
 </div>
--- wp-content/themes/pub/wporg-learn-2020/template-parts/content-workshop-single.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/content-workshop-single.php
@@ -12,7 +12,7 @@
 				<div class="col-4">
 					<?php get_template_part( 'template-parts/component', 'author' ); ?>
 				</div>
-				<p class="col-8">PHP Developer, Community Team deputy, WordPress Cape Town Meetup co-organiser, WordCamp Cape Town organiser, speaker, writer, podcaster. I like to get stuff done. Husband and father of two energetic boys. Gracie Jiu Jitsu for fun and stress release.</p>
+				<p class="col-8"><?php echo get_the_author_meta( 'description' ); ?></p>
 			</section>
 		</div>
 	</section>
