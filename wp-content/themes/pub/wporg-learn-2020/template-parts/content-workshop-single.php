04730e4d674b7943303514c77e784509f560f505
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-08-11 14:15:00 -0700

    Rename "author" to "presenter", other code tweaks
    "Author" means something specific in WordPress (the user who created
    a post), but in this case what we're displaying is the presenter(s)
    of the workshop.

--- wp-content/themes/pub/wporg-learn-2020/template-parts/content-workshop-single.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/content-workshop-single.php
@@ -1,5 +1,4 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<main class="site-main">
 	<section>
 		<div class="row align-middle between section-heading section-heading--with-space">
 			<h1 class="section-heading_title h2"><?php the_title(); ?></h1>
@@ -7,18 +6,21 @@
 		<hr>
 		<div class="workshop-page">
 			<?php the_content(); ?>
-			<?php foreach ( wporg_get_workshop_authors() as $author ) : ?>
-				<?php if( $author ) : ?>
-					<section class="row workshop-page_section"">
-						<div class="col-4">
-							<?php get_template_part( 'template-parts/component', 'author', 
-							array( 
-        						'author' => $author
-    						) ); ?>
-						</div>
-						<p class="col-8"><?php echo esc_html( $author->description ); ?></p>
-					</section>
-				<?php endif; ?>
+			<?php foreach ( wporg_get_workshop_presenters() as $presenter ) : ?>
+				<section class="row workshop-page_section"">
+					<div class="col-4">
+						<?php
+						get_template_part(
+							'template-parts/component',
+							'workshop-presenter',
+							array(
+								'presenter' => $presenter,
+							)
+						);
+						?>
+					</div>
+					<p class="col-8"><?php echo esc_html( $presenter->description ); ?></p>
+				</section>
 			<?php endforeach; ?>
 		</div>
 	</section>
