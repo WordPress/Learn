70b7308dd309b547eba6215981f0cb424aefd46d
dufresnesteven <steve.dufresne@automattic.com>
2020-08-12 10:13:50 +0900

    Move the image into the link.

--- wp-content/themes/pub/wporg-learn-2020/template-parts/component-video-grid-item.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-video-grid-item.php
@@ -10,10 +10,12 @@
 ?>
 
 <li class="col-4 video-grid_item">
-	<?php if( has_post_thumbnail() ) : ?>
-		<?php echo the_post_thumbnail( 'medium' ); ?>
-	<?php else : ?>
-		<div class="video-grid_item--no-image"></div>
-	<? endif; ?>
-	<a class="video-grid_item_link" href="<?php echo esc_url( get_the_permalink() ); ?>"><?php the_title(); ?></a>
+	<a class="video-grid_item_link" href="<?php echo esc_url( get_the_permalink() ); ?>">
+		<?php if( has_post_thumbnail() ) : ?>
+			<?php echo the_post_thumbnail( 'medium' ); ?>
+		<?php else : ?>
+			<div class="video-grid_item--no-image"></div>
+		<? endif; ?>
+		<?php the_title(); ?>
+	</a>
 </li>
