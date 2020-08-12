e7a3819c356582e8ab4e6709be5bfa47f0480abd
tellyworth <alex@automattic.com>
2020-08-12 16:47:59 +1000

    Fix syntax error and doubled semicolon
    Thanks @dd32 !

--- wp-content/themes/pub/wporg-learn-2020/template-parts/component-video-grid-item.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-video-grid-item.php
@@ -11,7 +11,7 @@
 
 <li class="col-4 video-grid_item">
 	<a class="video-grid_item_link" href="<?php echo esc_url( get_the_permalink() ); ?>">
-		<?php echo wporg_get_post_thumbnail( $post, 'medium' );; ?>
+		<?php echo wporg_get_post_thumbnail( $post, 'medium' ); ?>
 		<?php the_title(); ?>
 	</a>
 </li>
