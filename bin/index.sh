8c4ae085bef1741e87f4879b7a4fcb079aeecc75
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-08-13 16:11:14 -0700

    Disable lesson plan import in the local dev startup script

--- bin/index.sh
+++ bin/index.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Setup the environment
-npm run wp-env start 
+npm run wp-env start
 
 # Update wp configs
 npm run wp-env run cli wp config set JETPACK_DEV_DEBUG true
@@ -20,8 +20,9 @@ npm run wp-env run cli wp theme activate pub/wporg-learn-2020
 yarn
 
 ## Import lesson plans
-npm run wp-env run cli wp cron event run wporg_learn_manifest_import
-npm run wp-env run cli wp cron event run wporg_learn_markdown_import
+# Disabled since markdown import is currently disabled in the plugin.
+#npm run wp-env run cli wp cron event run wporg_learn_manifest_import
+#npm run wp-env run cli wp cron event run wporg_learn_markdown_import
 
 ## Activate jetpack modules
 npm run wp-env run cli wp jetpack module activate contact-form
