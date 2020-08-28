dbe805f4dde2adca1b1be8fc202feeaa51cfb19b
dufresnesteven <steve.dufresne@automattic.com>
2020-08-28 14:57:41 +0900

    Create a card based layout.

--- wp-content/themes/pub/wporg-learn-2020/search.php
+++ wp-content/themes/pub/wporg-learn-2020/search.php
@@ -33,44 +33,13 @@ get_header(); ?>
 			<header class="page-header">
 				<h1 class="h3"><?php echo esc_html( $search_query ); ?></h1>
 			</header><!-- .page-header -->
-
+			<div class="search-grid">
 			<?php
 			// Start the loop.
 			while ( have_posts() ) :
 				the_post();
-				?>
-
-			<article id="post-<?php the_ID(); ?>" <?php post_class( 'card' ); ?>>
-				<header class="entry-header">
-					<?php the_title( sprintf( '<h2 class="entry-title h4"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-				</header><!-- .entry-header -->
-
-				<?php if ( 'post' === get_post_type() ) : ?>
-					<div class="entry-meta"><?php entry_meta(); ?></div>
-				<?php endif; ?>
-
-				<div class="entry-summary">
-					<?php the_excerpt(); ?>
-				</div><!-- .entry-summary -->
-
-				<footer class="entry-footer">
-					<?php
-					edit_post_link(
-						sprintf(
-							/* translators: %s: Name of current post */
-							__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'wporg-learn' ),
-							get_the_title()
-						),
-						'<span class="edit-link">',
-						'</span>'
-					);
-					?>
-				</footer><!-- .entry-footer -->
-			</article><!-- #post-## -->
-
-				<?php
-				// End the loop.
-				endwhile;
+				get_template_part( 'template-parts/component', 'post-card' );
+			endwhile;
 
 			the_posts_pagination();
 
@@ -80,6 +49,7 @@ get_header(); ?>
 
 		endif;
 		?>
+		</div>
 	</main><!-- .site-main -->
 
 <?php
---
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-post-card-footer.php
@@ -0,0 +1,24 @@
+<?php
+
+use function WPOrg_Learn\Post_Meta\get_workshop_duration;
+
+$topics = wp_get_post_terms( $post->ID, 'topic', array( 'fields' => 'names' ) );
+$topics_string = implode( ', ', array_map( 'esc_html', $topics ) );
+$languages = esc_html( $post->video_language );
+$duration = get_workshop_duration( $post, 'string' );
+
+$post_tags = array(
+	'testimonial' => $topics_string,
+	'clock' => $duration,
+	'admin-site-alt3' => $languages,
+)
+
+?>
+<?php foreach ( $post_tags as $post_tag => $value ) : ?>
+	<?php if ( ! empty( $value ) ) : ?>
+		<div class="footer_item">
+			<i class="dashicons dashicons-<?php echo esc_attr( $post_tag ); ?>"></i>
+			<span><?php echo esc_attr( $value ); ?></span>
+		</div>
+	<?php endif; ?>
+<?php endforeach; ?>
