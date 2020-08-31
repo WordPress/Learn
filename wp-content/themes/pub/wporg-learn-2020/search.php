121d96333f06add6a1f85aabe9359ee7fc3cfe2e
dufresnesteven <steve.dufresne@automattic.com>
2020-08-31 11:17:36 +0900

    Bring the search page title in line with theme.

--- wp-content/themes/pub/wporg-learn-2020/search.php
+++ wp-content/themes/pub/wporg-learn-2020/search.php
@@ -18,38 +18,30 @@ $search_query = sprintf(
 get_header(); ?>
 
 	<main id="main" class="site-main type-page" role="main">
-		<div class="clearfix">
-			<div class="bbp-breadcrumb">
-				<p>
-					<a href="<?php echo esc_url( home_url() ); ?>" class="bbp-breadcrumb-home"><?php esc_html_e( 'Learn Home', 'wporg-learn' ); ?></a>
-					<span class="bbp-breadcrumb-sep">»</span>
-					<span class="bbp-breadcrumb-current"><?php echo esc_html( $search_query ); ?></span>
-				</p>
-			</div>
-		</div>
-
 		<?php if ( have_posts() ) : ?>
+			<section>
+				<div class="row align-middle between section-heading section-heading--with-space">
+					<h1 class="section-heading_title h2"><?php echo esc_html( $search_query ); ?></h1>
+				</div>
+				<hr>
+				<div class="search-grid">
+					<?php
+					// Start the loop.
+					while ( have_posts() ) :
+						the_post();
+						get_template_part( 'template-parts/component', 'post-card' );
+					endwhile;
 
-			<header class="page-header">
-				<h1 class="h3"><?php echo esc_html( $search_query ); ?></h1>
-			</header><!-- .page-header -->
-			<div class="search-grid">
-			<?php
-			// Start the loop.
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/component', 'post-card' );
-			endwhile;
+					the_posts_pagination();
 
-			the_posts_pagination();
+					// If no content, include the "No posts found" template.
+				else :
+					get_template_part( 'template-parts/content', 'none' );
 
-			// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-		</div>
+				endif;
+				?>
+			</div>
+		</section>
 	</main><!-- .site-main -->
 
 <?php
