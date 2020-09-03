434d3b079968a667caecd78a732926dd4f3615bf
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-09-02 22:08:41 -0700

    Add functions for getting lists of locales

--- wp-content/mu-plugins/pub/locales.php
+++ wp-content/mu-plugins/pub/locales.php
@@ -11,7 +11,7 @@ namespace {
 
 namespace WordPressdotorg\Locales {
 
-	use GP_Locales;
+	use GP_Locales, GP_Locale;
 
 	/**
 	 * Sets available languages to all possible locales.
@@ -28,4 +28,57 @@ namespace WordPressdotorg\Locales {
 		return $locales;
 	}
 	add_filter( 'get_available_languages', __NAMESPACE__ . '\set_available_languages', 10, 0 );
+
+	/**
+	 * Retrieves all available locales.
+	 *
+	 * @return GP_Locale[] Array of locale objects.
+	 */
+	function get_locales() {
+		wp_cache_add_global_groups( array( 'locale-associations' ) );
+
+		$wp_locales = wp_cache_get( 'locale-list', 'locale-associations' );
+		if ( false === $wp_locales ) {
+			$wp_locales = (array) $GLOBALS['wpdb']->get_col( 'SELECT locale FROM wporg_locales' );
+			wp_cache_set( 'locale-list', $wp_locales, 'locale-associations' );
+		}
+
+		$wp_locales[] = 'en_US';
+		$locales = array();
+
+		foreach ( $wp_locales as $locale ) {
+			$gp_locale = GP_Locales::by_field( 'wp_locale', $locale );
+			if ( ! $gp_locale ) {
+				continue;
+			}
+
+			$locales[ $locale ] = $gp_locale;
+		}
+
+		natsort( $locales );
+
+		return $locales;
+	}
+
+	/**
+	 * Get an array of locales with the locale code as key and the native name as value.
+	 *
+	 * @return array
+	 */
+	function get_locales_with_native_names() {
+		$locales = get_locales();
+
+		return wp_list_pluck( $locales, 'native_name', 'wp_locale' );
+	}
+
+	/**
+	 * Get an array of locales with the locale code as key and the English name as value.
+	 *
+	 * @return array
+	 */
+	function get_locales_with_english_names() {
+		$locales = get_locales();
+
+		return wp_list_pluck( $locales, 'english_name', 'wp_locale' );
+	}
 }
