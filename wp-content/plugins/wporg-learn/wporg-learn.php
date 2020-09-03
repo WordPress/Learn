89d34c3f96274a703bba54831e11b1ab526cfbe2
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-09-03 15:31:29 -0700

    Add a namespace to the root plugin file, load includes on a hook

--- wp-content/plugins/wporg-learn/wporg-learn.php
+++ wp-content/plugins/wporg-learn/wporg-learn.php
@@ -8,25 +8,37 @@
  * License:     GPLv2 or later
  */
 
-require_once dirname( __FILE__ ) . '/inc/blocks.php';
-require_once dirname( __FILE__ ) . '/inc/class-markdown-import.php';
-require_once dirname( __FILE__ ) . '/inc/class-shortcodes.php';
-require_once dirname( __FILE__ ) . '/inc/post-meta.php';
-require_once dirname( __FILE__ ) . '/inc/post-type.php';
-require_once dirname( __FILE__ ) . '/inc/taxonomy.php';
+namespace WPOrg_Learn;
+
+defined( 'WPINC' ) || die();
 
 /**
  * Actions and filters.
  */
-add_action( 'admin_enqueue_scripts', 'wporg_learn_register_thirdparty_assets', 9 );
-add_action( 'wp_enqueue_scripts', 'wporg_learn_register_thirdparty_assets', 9 );
+add_action( 'plugins_loaded', __NAMESPACE__ . '\load_files' );
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\register_thirdparty_assets', 9 );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_thirdparty_assets', 9 );
+
+/**
+ * Load the other PHP files for the plugin.
+ *
+ * @return void
+ */
+function load_files() {
+	require_once dirname( __FILE__ ) . '/inc/blocks.php';
+	require_once dirname( __FILE__ ) . '/inc/class-markdown-import.php';
+	require_once dirname( __FILE__ ) . '/inc/class-shortcodes.php';
+	require_once dirname( __FILE__ ) . '/inc/post-meta.php';
+	require_once dirname( __FILE__ ) . '/inc/post-type.php';
+	require_once dirname( __FILE__ ) . '/inc/taxonomy.php';
+}
 
 /**
  * Register scripts and styles for 3rd party libraries.
  *
  * @return void
  */
-function wporg_learn_register_thirdparty_assets() {
+function register_thirdparty_assets() {
 	wp_register_script(
 		'select2',
 		plugins_url( '/3rd-party/selectWoo/js/selectWoo.min.js', __FILE__ ),
