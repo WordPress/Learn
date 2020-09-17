630d27163241df8bc488f419e283ce0fbb621bea
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-09-17 12:08:07 -0700

    Workshops: Add columns for language and captions to list table (#119)
    Fixes #102

--- wp-content/plugins/wporg-learn/inc/post-type.php
+++ wp-content/plugins/wporg-learn/inc/post-type.php
@@ -2,12 +2,16 @@
 
 namespace WPOrg_Learn\Post_Type;
 
+use function WordPressdotorg\Locales\get_locale_name_from_code;
+
 defined( 'WPINC' ) || die();
 
 /**
  * Actions and filters.
  */
 add_action( 'init', __NAMESPACE__ . '\register' );
+add_filter( 'manage_wporg_workshop_posts_columns', __NAMESPACE__ . '\add_workshop_list_table_columns' );
+add_action( 'manage_wporg_workshop_posts_custom_column', __NAMESPACE__ . '\render_workshop_list_table_columns', 10, 2 );
 
 /**
  * Register all post types.
@@ -217,4 +221,51 @@ function register_workshop() {
 	);
 
 	register_post_type( 'wporg_workshop', $args );
+}
+
+/**
+ * Add additional columns to the post list table for workshops.
+ *
+ * @param array $columns
+ *
+ * @return array
+ */
+function add_workshop_list_table_columns( $columns ) {
+	$columns = array_slice( $columns, 0, -2, true )
+				+ array( 'video_language' => __( 'Language', 'wporg-learn' ) )
+				+ array( 'video_caption_language' => __( 'Captions', 'wporg-learn' ) )
+				+ array_slice( $columns, -2, 2, true );
+
+	return $columns;
+}
+
+/**
+ * Render the cell contents for the additional columns in the post list table for workshops.
+ *
+ * @param string $column_name
+ * @param int    $post_id
+ *
+ * @return void
+ */
+function render_workshop_list_table_columns( $column_name, $post_id ) {
+	$post = get_post( $post_id );
+
+	switch ( $column_name ) {
+		case 'video_language':
+			echo esc_html( get_locale_name_from_code( $post->video_language, 'english' ) );
+			break;
+		case 'video_caption_language':
+			$captions = get_post_meta( $post->ID, 'video_caption_language' );
+
+			echo esc_html( implode(
+				', ',
+				array_map(
+					function( $caption_lang ) {
+						return get_locale_name_from_code( $caption_lang, 'english' );
+					},
+					$captions
+				)
+			) );
+			break;
+	}
 }
