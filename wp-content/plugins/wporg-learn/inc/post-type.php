f0120634d0898473dd0627e6672df8a4ea2b1b43
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-10-01 17:08:27 -0700

    Enable injecting content into workshop template

--- wp-content/plugins/wporg-learn/inc/post-type.php
+++ wp-content/plugins/wporg-learn/inc/post-type.php
@@ -116,6 +116,68 @@ function register_workshop() {
 		'filter_items_list'     => __( 'Filter Workshops list', 'wporg_learn' ),
 	);
 
+	$args = array(
+		'label'               => __( 'Workshop', 'wporg_learn' ),
+		'description'         => __( 'WordPress.org Training Workshop', 'wporg_learn' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'comments', 'revisions', 'custom-fields', 'thumbnail', 'excerpt' ),
+		'taxonomies'          => array( 'level', 'topic' ),
+		'hierarchical'        => true,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'has_archive'         => 'workshops',
+		'menu_position'       => 6,
+		'menu_icon'           => 'dashicons-desktop',
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+		'show_in_rest'        => true,
+		'template_lock'       => 'all',
+		'rewrite'             => array( 'slug' => 'workshop' ),
+		'template'            => generate_workshop_template_structure(),
+	);
+
+	register_post_type( 'wporg_workshop', $args );
+}
+
+/**
+ * Create an array representation of a workshop's content template, with or without real content.
+ *
+ * @param array $blurbs {
+ *     The parts of the template that can be populated with real content.
+ *
+ *     @type string $description
+ *     @type string $learning_objectives
+ *     @type string $comprehension_questions
+ * }
+ *
+ * @return array
+ */
+function generate_workshop_template_structure( array $blurbs = array() ) {
+	$blurbs = wp_parse_args(
+		$blurbs,
+		array(
+			'description'             => '',
+			'learning_objectives'     => '',
+			'comprehension_questions' => '',
+		)
+	);
+
+	foreach ( array( 'learning_objectives', 'comprehension_questions' ) as $key ) {
+		// Turn separate lines into list items.
+		$content = str_replace( array( "\r\n", "\r" ), "\n", $blurbs[ $key ] );
+		$split   = explode( "\n", $content );
+		$split   = array_filter( array_map( 'trim', $split ) );
+
+		if ( ! empty( $split ) ) {
+			$blurbs[ $key ] = '<li>' . implode( '</li><li>', $split ) . '</li>';
+		}
+	}
+
 	$template = array(
 		array(
 			'core-embed/wordpress-tv',
@@ -133,6 +195,7 @@ function register_workshop() {
 							'core/paragraph',
 							array(
 								'placeholder' => __( 'Describe what the workshop is about.', 'wporg-learn' ),
+								'content'     => $blurbs['description'],
 							),
 						),
 						array(
@@ -147,6 +210,7 @@ function register_workshop() {
 							array(
 								'className' => 'workshop-page_list',
 								'ordered'   => true,
+								'values'    => $blurbs['learning_objectives'],
 							),
 						),
 						array(
@@ -160,6 +224,7 @@ function register_workshop() {
 							'core/list',
 							array(
 								'className' => 'workshop-page_list',
+								'values'    => $blurbs['comprehension_questions'],
 							),
 						),
 					),
@@ -198,32 +263,7 @@ function register_workshop() {
 		array( 'core/separator' ),
 	);
 
-	$args = array(
-		'label'               => __( 'Workshop', 'wporg_learn' ),
-		'description'         => __( 'WordPress.org Training Workshop', 'wporg_learn' ),
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'editor', 'comments', 'revisions', 'custom-fields', 'thumbnail', 'excerpt' ),
-		'taxonomies'          => array( 'level', 'topic' ),
-		'hierarchical'        => true,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'has_archive'         => 'workshops',
-		'menu_position'       => 6,
-		'menu_icon'           => 'dashicons-desktop',
-		'show_in_admin_bar'   => true,
-		'show_in_nav_menus'   => true,
-		'can_export'          => true,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'capability_type'     => 'page',
-		'show_in_rest'        => true,
-		'template_lock'       => 'all',
-		'rewrite'             => array( 'slug' => 'workshop' ),
-		'template'            => $template,
-	);
-
-	register_post_type( 'wporg_workshop', $args );
+	return $template;
 }
 
 /**
