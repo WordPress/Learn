bfabc325f2aa70cdd0c6343ef189f960c2e16bf3
Ian Dunn <ian@iandunn.name>
2021-03-19 16:42:08 -0700

    Plugin: Stop tracking `build/` to improve devex. (#198)
    Fixes #197
    
    Co-authored-by: Corey McKrill <916023+coreymckrill@users.noreply.github.com>

--- wp-content/plugins/wporg-learn/build/block-styles.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n', 'wp-polyfill'), 'version' => 'f3d3893690806d470b92672dd18d80ea');
--- wp-content/plugins/wporg-learn/build/event.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-date', 'wp-polyfill'), 'version' => '09ab956fd218879057b719ac22233c5f');
