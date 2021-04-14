f24f0b8ce88d968d2202f189bfb070ea9c56f29a
Dion Hulse <dion@wordpress.org>
2021-04-14 06:10:44 +0000

    Import rtlcss from wporg base theme

--- wp-content/themes/pub/wporg-learn-2020/Gruntfile.js
+++ wp-content/themes/pub/wporg-learn-2020/Gruntfile.js
@@ -64,6 +64,68 @@ module.exports = function( grunt ) {
 			options: { signature: false }
 		},
 
+		rtlcss: {
+			options: {
+				// rtlcss options.
+				opts: {
+					clean: false,
+					processUrls: { atrule: true, decl: false },
+					stringMap: [
+						{
+							name: 'import-rtl-stylesheet',
+							priority: 10,
+							exclusive: true,
+							search: [ '.css' ],
+							replace: [ '-rtl.css' ],
+							options: {
+								scope: 'url',
+								ignoreCase: false
+							}
+						} // phpcs:ignore Generic.WhiteSpace.ScopeIndent.IncorrectExact
+					]
+				},
+				saveUnmodified: false,
+				plugins: [
+					{
+						name: 'swap-dashicons-left-right-arrows',
+						priority: 10,
+						directives: {
+							control: {},
+							value: []
+						},
+						processors: [
+							{
+								expr: /content/im,
+								action: function( prop, value ) {
+									if ( value === '"\\f141"' ) { // dashicons-arrow-left.
+										value = '"\\f139"';
+									} else if ( value === '"\\f340"' ) { // dashicons-arrow-left-alt.
+										value = '"\\f344"';
+									} else if ( value === '"\\f341"' ) { // dashicons-arrow-left-alt2.
+										value = '"\\f345"';
+									} else if ( value === '"\\f139"' ) { // dashicons-arrow-right.
+										value = '"\\f141"';
+									} else if ( value === '"\\f344"' ) { // dashicons-arrow-right-alt.
+										value = '"\\f340"';
+									} else if ( value === '"\\f345"' ) { // dashicons-arrow-right-alt2.
+										value = '"\\f341"';
+									}
+									return { prop: prop, value: value };
+								}
+							} // phpcs:ignore Generic.WhiteSpace.ScopeIndent.IncorrectExact
+						]
+					} // phpcs:ignore Generic.WhiteSpace.ScopeIndent.IncorrectExact
+				]
+			},
+			dynamic: {
+				expand: true,
+				cwd: 'css/',
+				dest: 'css/',
+				ext: '-rtl.css',
+				src: ['**/style.css']
+			}
+		},
+
 		watch: {
 			css: {
 				files: ['**/*.scss', '../wporg/css/**/*scss'],
@@ -77,11 +139,12 @@ module.exports = function( grunt ) {
 	}
 
 	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-rtlcss' );
 	grunt.loadNpmTasks( 'grunt-postcss' );
 	grunt.loadNpmTasks( 'grunt-sass-globbing' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-	grunt.registerTask( 'css', [ 'sass_globbing', 'sass', 'postcss' ] );
+	grunt.registerTask( 'css', [ 'sass_globbing', 'sass', 'postcss', 'rtlcss:dynamic' ] );
 
 	grunt.registerTask( 'default', [ 'css' ] );
 	grunt.registerTask( 'build', [ 'css' ] ); // Automatically runs "production" steps
