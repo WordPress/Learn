b1e48330e2270fe1297d61877b38412084a5a8af
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2021-10-08 17:00:27 -0700

    Add series navigation component to single lesson plan view

--- wp-content/themes/pub/wporg-learn-2020/template-parts/content-single.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/content-single.php
@@ -73,6 +73,9 @@
 					</div>
 				</div>
 			</aside>
-			</div>
+		</div>
+		<?php if ( is_object_in_term( get_the_ID(), 'wporg_workshop_series' ) ) : ?>
+			<?php get_template_part( 'template-parts/component', 'series-navigation' ); ?>
+		<?php endif; ?>
 	</section>
 </article><!-- #post-## -->
