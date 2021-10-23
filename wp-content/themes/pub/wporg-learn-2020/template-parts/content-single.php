f9eb27e48f5c15e3d920293b42df1279ecda336d
Jonathan Bossenger <jonathanbossenger@gmail.com>
2021-10-23 19:33:42 +0200

    Wrap the print view link in a paragraph tag
    Sorts out the layout issue

--- wp-content/themes/pub/wporg-learn-2020/template-parts/content-single.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/content-single.php
@@ -65,7 +65,7 @@
 						</li> -->
 					</ul>
 					<div class="lp-print">
-						<a href="#" onclick="window.print();"><?php esc_html_e( 'Print view', 'wporg-learn' ); ?></a>
+						<p><a href="#" onclick="window.print();"><?php esc_html_e( 'Print view', 'wporg-learn' ); ?></a></p>
 					</div>
 					<div class="lp-suggestion">
 						<h2 class="lp-suggestion_title h4"><?php esc_html_e( 'Suggestions', 'wporg-learn' ); ?></h2>
