0684f367e504e22e6e79d0360100222c3b5997c7
Alex Stine <alex.stine@yourtechadvisors.com>
2022-01-14 16:05:32 -0500

    Accessibility fixes for single Workshops (#301)

--- wp-content/themes/pub/wporg-learn-2020/functions.php
+++ wp-content/themes/pub/wporg-learn-2020/functions.php
@@ -1039,8 +1039,8 @@ function wporg_learn_register_sidebars() {
 		array(
 			'name'          => __( 'Workshops', 'wporg-learn' ),
 			'id'            => 'wporg-learn-workshops',
-			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</aside>',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
 			'before_title'  => '<h4 class="widget-title">',
 			'after_title'   => '</h4>',
 		)
