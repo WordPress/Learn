f92d551bfc3c533a69e6242ab1c75e67e28d77e9
Dion Hulse <dion@wordpress.org>
2022-02-15 07:17:11 +0000

    Add a quick and dirty sync script

---
+++ bin/sync-to-production.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rm -rf /tmp/learn
+
+git clone https://github.com/WordPress/learn.git /tmp/learn
+
+svn co https://meta.svn.wordpress.org/sites/trunk/wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2020 /tmp/learn/meta-theme
+svn co https://meta.svn.wordpress.org/sites/trunk/wordpress.org/public_html/wp-content/plugins/wporg-learn /tmp/learn/meta-plugin
+
+cd /tmp/learn
+
+# Install
+yarn
+
+# Install v2
+composer install
+
+# Build
+yarn workspaces run build
+
+# Sync git to SVN
+rm -rf meta-theme/*
+cp -r wp-content/themes/pub/wporg-learn-2020/* meta-theme
+svn st meta-theme/ | grep ^? | cut -c2- | xargs -I% svn add %
+svn st meta-theme/ | grep ^! | cut -c2- | xargs -I% svn rm %
+
+rm -rf meta-plugin/*
+cp -r wp-content/plugins/wporg-learn/* meta-plugin
+svn st meta-plugin/ | grep ^? | cut -c2- | xargs -I% svn add %
+svn st meta-plugin/ | grep ^! | cut -c2- | xargs -I% svn rm %
+
+# Print diff.
+svn st meta-*
+
+echo "svn diff /tmp/learn/meta-* to view diff."
+
+# pause
+echo "Press any key to commit"
+read
+
+COMMIT=`git log | head -1 | cut -d' ' -f2`
+MSG="Learn: Sync with git WordPress/learn@$COMMIT"
+
+svn ci meta-plugin meta-theme -m "$MSG"
