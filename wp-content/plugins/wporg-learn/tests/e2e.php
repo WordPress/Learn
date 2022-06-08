1c6c6bfa8b62cbd7a1fe076616600720b595a177
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-06-09 00:22:38 +0200

    Fix PHPCS related errors in tests (#773)

--- wp-content/plugins/wporg-learn/tests/e2e.php
+++ wp-content/plugins/wporg-learn/tests/e2e.php
@@ -5,14 +5,15 @@
  * ⚠️ These tests run against the production database and object cache on learn.w.org and profiles.w.org.
  * Make sure that any modifications are hardcoded to only affect test sites and test user accounts.
  *
- * usage: wp eval-file e2e.php test_name
+ * Usage: wp eval-file e2e.php test_name
  */
 
 namespace WordCamp\Participation_Notifier\Tests;
 use WPOrg_Learn\Profiles as Learn_Profiles;
 use Exception, WP_User, WP_Post;
 
-ini_set( 'display_errors', 'On' ); // won't do anything if fatal errors
+// won't do anything if fatal errors
+ini_set( 'display_errors', 'On' ); //phpcs:ignore
 
 if ( 'staging' !== wp_get_environment_type() || 'cli' !== php_sapi_name() ) {
 	die( 'Error: Wrong environment.' );
@@ -24,6 +25,11 @@ const TEST_COURSE_ID = 17240;
 /** @var array $args */
 main( $args[0] );
 
+/**
+ * @param Case $case
+ *
+ * @return void
+ */
 function main( $case ) {
 	switch_to_blog( 7 ); // learn.w.org
 	$user   = get_user_by( 'slug', TEST_USERNAME );
@@ -34,15 +40,22 @@ function main( $case ) {
 		call_user_func( __NAMESPACE__ . "\\test_$case", $user, $course );
 
 	} catch ( Exception $exception ) {
-		echo $exception->getMessage();
+		echo $exception->getMessage(); //phpcs:ignore
 
 	} finally {
 		restore_current_blog();
 	}
 }
 
+/**
+ *
+ * @param WP_User $user
+ * @param WP_Post $course
+ *
+ * @return void
+ */
 function test_add( WP_User $user, WP_Post $course ) {
 	Learn_Profiles\add_course_completed_activity( 'complete', $user->ID, $course->ID );
 
-	echo "\nThere should be a course confirmation activity on https://profiles.wordpress.org/$user->user_nicename/ \n";
+	echo "\nThere should be a course confirmation activity on https://profiles.wordpress.org/$user->user_nicename/ \n"; //phpcs:ignore
 }
