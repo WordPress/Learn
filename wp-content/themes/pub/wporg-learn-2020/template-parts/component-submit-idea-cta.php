f2f08f9bb4ab14088731c87e0261b99aac0312a0
Hugh Lashbrooke <1543143+hlashbrooke@users.noreply.github.com>
2022-07-14 14:24:45 +1200

    Front page updates
    Updating links and references to workshops and social learning spaces on the front page

--- wp-content/themes/pub/wporg-learn-2020/template-parts/component-submit-idea-cta.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-submit-idea-cta.php
@@ -15,6 +15,6 @@ $args = wp_parse_args( $args );
 	<?php if ( isset( $args['icon'] ) ) : ?>
 		<div aria-hidden="true" class="content-icon"><span class="dashicons dashicons-<?php echo esc_attr( $args['icon'] ); ?>"></span></div>
 	<?php endif; ?>
-	<h2><?php esc_html_e( 'Have an Idea for a Workshop? Let us know!', 'wporg-learn' ); ?></h2>
-	<a class="button button-primary button-large" href="https://learn.wordpress.org/workshop-presenter-application/"><span aria-hidden="true"><?php esc_html_e( 'Submit an Idea', 'wporg-learn' ); ?></span><span class="screen-reader-text"><?php esc_html_e( 'Submit Workshop Idea', 'wporg-learn' ); ?></span></a>
+	<h2><?php esc_html_e( 'Have an Idea for a Tutorial? Let us know!', 'wporg-learn' ); ?></h2>
+	<a class="button button-primary button-large" href="https://learn.wordpress.org/tutorial-presenter-application/"><span aria-hidden="true"><?php esc_html_e( 'Submit an Idea', 'wporg-learn' ); ?></span><span class="screen-reader-text"><?php esc_html_e( 'Submit Tutorial Idea', 'wporg-learn' ); ?></span></a>
 </section>
