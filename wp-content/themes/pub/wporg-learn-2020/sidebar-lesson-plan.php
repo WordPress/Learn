6cdd7e0e564834472de73e12ef3c141d12c98aad
Adam Wood <adam.wood@automattic.com>
2022-07-22 13:58:02 +1200

    Add print button to Lesson Plan sidebar (#820)
    * Add print button to Lesson Plan sidebar
    
    Hides some elements of sidebar in print view.
    
    * Revert to using existing lesson plan print button
    
    * Improve accessibility and CSS performance of lesson plan sidebar actions

--- wp-content/themes/pub/wporg-learn-2020/sidebar-lesson-plan.php
+++ wp-content/themes/pub/wporg-learn-2020/sidebar-lesson-plan.php
@@ -22,34 +22,36 @@ namespace WordPressdotorg\Theme;
 			?>
 		</ul>
 
-		<ul class="lp-links">
+		<ul class="lp-actions">
 			<?php if ( $post->slides_view_url ) : ?>
 				<li>
-					<a href="<?php echo esc_attr( $post->slides_view_url ); ?>" target="_blank">
-						<span class="dashicons dashicons-admin-page"></span>
+					<a class="lp-action" href="<?php echo esc_attr( $post->slides_view_url ); ?>" target="_blank">
+						<span class="dashicons dashicons-admin-page" aria-hidden="true"></span>
 						<?php esc_html_e( 'View Lesson Plan Slides', 'wporg-learn' ); ?>
 					</a>
 				</li>
 			<?php endif; ?>
 			<?php if ( $post->slides_download_url ) : ?>
 				<li>
-					<a href="<?php echo esc_attr( $post->slides_download_url ); ?>">
-						<span class="dashicons dashicons-download"></span>
+					<a class="lp-action" href="<?php echo esc_attr( $post->slides_download_url ); ?>">
+						<span class="dashicons dashicons-download" aria-hidden="true"></span>
 						<?php esc_html_e( 'Download Lesson Slides', 'wporg-learn' ); ?>
 					</a>
 				</li>
 			<?php endif; ?>
-			<!-- <li>
-				<a href="#" target="_blank">
-					<span class="dashicons dashicons-admin-post"></span>
+			<li>
+				<button class="lp-action" onclick="window.print()">
+					<span class="dashicons dashicons-printer" aria-hidden="true"></span>
 					<?php esc_html_e( 'Print Lesson Plan', 'wporg-learn' ); ?>
-				</a>
-			</li> -->
+				</button>
+			</li>
 		</ul>
 	</div>
-	<?php
-	if ( is_active_sidebar( 'wporg-learn-lesson-plans' ) ) :
-		dynamic_sidebar( 'wporg-learn-lesson-plans' );
-	endif;
-	?>
+	<div class="lp-sidebar-dynamic">
+		<?php
+		if ( is_active_sidebar( 'wporg-learn-lesson-plans' ) ) :
+			dynamic_sidebar( 'wporg-learn-lesson-plans' );
+		endif;
+		?>
+	</div>
 </aside>
