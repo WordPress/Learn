8ce7090b4ef8745a76f08d0a323bb0364c40b6ed
Courtney Robertson <5519705+courtneyr-dev@users.noreply.github.com>
2022-09-30 12:18:52 -0400

    Lesson plan pattern (#979)
    * spaces to tabs & final new line

--- wp-content/themes/pub/wporg-learn-2020/patterns/lesson-plan.php
+++ wp-content/themes/pub/wporg-learn-2020/patterns/lesson-plan.php
@@ -10,9 +10,9 @@
 ?>
 <!-- wp:quote -->
 <blockquote class="wp-block-quote">
-    <!-- wp:paragraph -->
-    <p>A short paragraph explaining what is covered in the lesson plan. This should be text that can be copied and used in a meetup or workshop description.</p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>A short paragraph explaining what is covered in the lesson plan. This should be text that can be copied and used in a meetup or workshop description.</p>
+	<!-- /wp:paragraph -->
 </blockquote>
 <!-- /wp:quote -->
 
@@ -26,25 +26,25 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Objective 1</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Objective 1</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Objective 2</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Objective 2</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
 <!-- wp:quote -->
 <blockquote class="wp-block-quote">
-    <!-- wp:paragraph -->
-    <p>It’s required that you include a bulleted list of objective(s) for each lesson plan. See Learning Objectives Easy Generator for help creating these. </p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>It’s required that you include a bulleted list of objective(s) for each lesson plan. See Learning Objectives Easy Generator for help creating these. </p>
+	<!-- /wp:paragraph -->
 
-    <!-- wp:paragraph -->
-    <p>Objectives should be worded as actions that the participant can do once they’ve finished. See&nbsp;<a href="http://www.fresnostate.edu/academics/oie/documents/assesments/Blooms%20Level.pdf">Bloom's Taxonomy of Action Verbs</a>&nbsp;(PDF) as a reference. Avoid using words like "know," "understand," "be introduced to," etc. There should be one assessment item for each objective (see below).</p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>Objectives should be worded as actions that the participant can do once they’ve finished. See&nbsp;<a href="http://www.fresnostate.edu/academics/oie/documents/assesments/Blooms%20Level.pdf">Bloom's Taxonomy of Action Verbs</a>&nbsp;(PDF) as a reference. Avoid using words like "know," "understand," "be introduced to," etc. There should be one assessment item for each objective (see below).</p>
+	<!-- /wp:paragraph -->
 </blockquote>
 <!-- /wp:quote -->
 
@@ -58,13 +58,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Skill 1</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Skill 1</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Skill 2</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Skill 2</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -74,13 +74,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Experience with HTML and CSS</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Experience with HTML and CSS</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Completed the Basic WordPress Concepts lesson</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Completed the Basic WordPress Concepts lesson</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -90,13 +90,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Question 1</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Question 1</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Question 2</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Question 2</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -110,13 +110,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Do you want to makes changes to your theme yourself?</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Do you want to makes changes to your theme yourself?</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Do you know how to write CSS?</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Do you know how to write CSS?</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -126,13 +126,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Item 1</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Item 1</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Item 2</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Item 2</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -146,13 +146,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>A local install of WordPress</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>A local install of WordPress</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>The files for the TwentySixteen theme</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>The files for the TwentySixteen theme</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -162,13 +162,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Note 1</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Note 1</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Note 2</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Note 2</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -182,21 +182,21 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Participants may need to download the TwentySixteen theme before beginning</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Participants may need to download the TwentySixteen theme before beginning</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>What to do if there’s no projector or internet available</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>What to do if there’s no projector or internet available</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>What to do if a participant doesn’t have the necessary set up</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>What to do if a participant doesn’t have the necessary set up</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>How to handle different opinions about the topic</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>How to handle different opinions about the topic</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -206,17 +206,17 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>First do this</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>First do this</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Then move on to this</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Then move on to this</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Finish with this</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Finish with this</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -230,17 +230,17 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Talk about what a theme is</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Talk about what a theme is</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Demonstrate how to install and activate a theme</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Demonstrate how to install and activate a theme</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Practice exercises to have participants find and install a theme on their own site</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Practice exercises to have participants find and install a theme on their own site</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -258,21 +258,21 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Short point or step of the exercise</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Short point or step of the exercise</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>And another one</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>And another one</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
 <!-- wp:quote -->
 <blockquote class="wp-block-quote">
-    <!-- wp:paragraph -->
-    <p>These are short or specific activities that help participants practice certain components of the lesson. They should not be fully scripted exercises, but rather something that participants could do on their own. For example, you can create an exercise based on one step of the Example Lesson.</p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>These are short or specific activities that help participants practice certain components of the lesson. They should not be fully scripted exercises, but rather something that participants could do on their own. For example, you can create an exercise based on one step of the Example Lesson.</p>
+	<!-- /wp:paragraph -->
 </blockquote>
 <!-- /wp:quote -->
 
@@ -290,21 +290,21 @@
 
 <!-- wp:list {"ordered":true} -->
 <ol>
-    <!-- wp:list-item -->
-    <li>Option</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Option</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Option</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Option</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Option</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Option</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Option</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Option</li>
+	<!-- /wp:list-item -->
 </ol>
 <!-- /wp:list -->
 
@@ -314,9 +314,9 @@
 
 <!-- wp:quote -->
 <blockquote class="wp-block-quote">
-    <!-- wp:paragraph -->
-    <p>A few questions to ask participants to evaluate their retention of the material presented. They should be a measure of whether the objectives were reached. Consider having a question for each objective.</p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>A few questions to ask participants to evaluate their retention of the material presented. They should be a measure of whether the objectives were reached. Consider having a question for each objective.</p>
+	<!-- /wp:paragraph -->
 </blockquote>
 <!-- /wp:quote -->
 
@@ -326,13 +326,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Resource 1</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Resource 1</li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li>Resource 2</li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Resource 2</li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -346,13 +346,13 @@
 
 <!-- wp:list -->
 <ul>
-    <!-- wp:list-item -->
-    <li>Link to information on the <a data-external="true" href="https://wordpress.org/support/">Support Docs</a> or <a data-external="true" href="https://developer.wordpress.org/">Developer Resources</a></li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li>Link to information on the <a data-external="true" href="https://wordpress.org/support/">Support Docs</a> or <a data-external="true" href="https://developer.wordpress.org/">Developer Resources</a></li>
+	<!-- /wp:list-item -->
 
-    <!-- wp:list-item -->
-    <li><a data-external="true" href="https://developer.wordpress.org/themes/">Theme Review Team's Handbook</a> or <a href="https://make.wordpress.org/plugins/handbook/">Plugin Review Team's Handbook</a></li>
-    <!-- /wp:list-item -->
+	<!-- wp:list-item -->
+	<li><a data-external="true" href="https://developer.wordpress.org/themes/">Theme Review Team's Handbook</a> or <a href="https://make.wordpress.org/plugins/handbook/">Plugin Review Team's Handbook</a></li>
+	<!-- /wp:list-item -->
 </ul>
 <!-- /wp:list -->
 
@@ -362,9 +362,9 @@
 
 <!-- wp:quote -->
 <blockquote class="wp-block-quote">
-    <!-- wp:paragraph -->
-    <p>An example of how the lesson plan can be implemented. Written in script form as one possible way an presenter might use this lesson plan at an event, with screenshots and instructions if necessary.</p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>An example of how the lesson plan can be implemented. Written in script form as one possible way an presenter might use this lesson plan at an event, with screenshots and instructions if necessary.</p>
+	<!-- /wp:paragraph -->
 </blockquote>
 <!-- /wp:quote -->
 
@@ -374,9 +374,9 @@
 
 <!-- wp:quote {"className":"is-style-default"} -->
 <blockquote class="wp-block-quote is-style-default">
-    <!-- wp:paragraph -->
-    <p>You will likely need to break the Example Lesson down into multiple sections.</p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>You will likely need to break the Example Lesson down into multiple sections.</p>
+	<!-- /wp:paragraph -->
 </blockquote>
 <!-- /wp:quote -->
 
@@ -391,3 +391,4 @@
 <!-- wp:paragraph -->
 <p></p>
 <!-- /wp:paragraph -->
+<p></p> 
