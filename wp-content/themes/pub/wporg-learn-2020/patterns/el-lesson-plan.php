da066471d64ad531d2a86c1985712a882f4a4f27
Vagelis Papaioannou <hello@vagelis.dev>
2022-10-01 16:27:02 +0300

    Greek Lesson plan block pattern

---
+++ wp-content/themes/pub/wporg-learn-2020/patterns/el-lesson-plan.php
@@ -0,0 +1,391 @@
+<?php
+
+/**
+ * Title: Greek Lesson Plan Content
+ * Slug: wporg-learn-2020/lesson-plan/greek
+ * Block Types: core/post-content
+ * Post Types: lesson-plan
+ * Categories: featured, text
+ */
+
+?>
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Μια σύντομη παράγραφος που εξηγεί τι καλύπτεται στο πλάνο μαθήματος. Αυτό θα πρέπει να είναι κείμενο που μπορεί να αντιγραφεί και να χρησιμοποιηθεί σε μια ομάδα συνάντησης ή εργαστηρίου.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Στόχοι</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Μετά την ολοκλήρωση αυτού του μαθήματος, οι συμμετέχοντες θα είναι σε θέση:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Στόχος 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Στόχος 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Απαιτείται να συμπεριλάβετε μια λίστα με κουκκίδες με στόχους για κάθε πλάνο μαθήματος. Ανατρέξτε στο Learning Objectives Easy Generator για βοήθεια στη δημιουργία αυτών.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Οι στόχοι πρέπει να διατυπώνονται ως ενέργειες που μπορούν να κάνουν οι συμμετέχοντες μόλις ολοκληρώσουν το μάθημα. Αποφύγετε τη χρήση λέξεων όπως «γνωρίζω», «καταλαβαίνω», «παρουσιάζομαι», κ.λπ. επίσης Θα πρέπει να υπάρχει ένα στοιχείο αξιολόγησης για κάθε στόχο (βλ. παρακάτω).</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Προαπαιτούμενες δεξιότητες</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Οι συμμετέχοντες θα αξιοποιήσουν στο έπακρο αυτό το μάθημα εάν έχουν εξοικείωση με:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Δεξιότητα 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Δεξιότητα 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><em>Παράδειγμα:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Εμπειρία με HTML και CSS</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Ολοκληρώθηκε το βασικό μάθημα WordPress</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Ερωτήσεις Ετοιμότητας</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Ερώτηση 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Ερώτηση 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Μια λίστα ερωτήσεων για τους συμμετέχοντες ώστε να δουν εάν έχουν το υπόβαθρο και τις απαραίτητες δεξιότητες να κατανοήσουν το μάθημα.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Παράδειγμα:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Θέλετε να κάνετε αλλαγές σε ένα θέμα εμφάνισης WordPress μόνοι σας;</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Ξέρετε να γράφετε CSS;</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Απαιτούμενα</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Απαιτούμενo 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Απαιτούμενo 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Μια λίστα αρχείων, πόρων, εξοπλισμού ή άλλου υλικού που θα χρειαστεί ο παρουσιαστής για το μάθημα.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Παράδειγμα:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Μια τοπική εγκατάσταση του WordPress</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Τα αρχεία το θέματος εμφάνισης Twenty Sixteen</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Σημειώσεις για τον παρουσιαστή</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Σημειώση 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Σημειώση 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Μια λίστα με εύχρηστες συμβουλές ή άλλες πληροφορίες για τον παρουσιαστή.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Παράδειγμα:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Οι συμμετέχοντες μπορεί να χρειαστεί να κατεβάσουν το θέμα εμφάνισης Twenty Sixteen πριν ξεκινήσουν</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Τι να κάνετε εάν δεν υπάρχει διαθέσιμος προβολέας ή Διαδίκτυο</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Τι να κάνετε εάν ένας συμμετέχων δεν έχει τις απαραίτητες ρυθμίσεις</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Πώς να χειριστείτε διαφορετικές απόψεις για το θέμα</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Πλάνο μαθήματος</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Πρώτα κάντε αυτό</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Μετα εκείνο</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Και τελειώστε με αυτό</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Το πλανο μαθήματος. Ένα περίγραμα λειτουργεί καλά.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Παράδειγμα:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Μιλήστε για το τι είναι ένα θέμα εμφάνισης</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Δείξτε πώς να εγκαταστήσετε και να ενεργοποιήσετε ένα θέμα εμφάνισης</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Πραγματοποιήστε ασκήσεις με σκοπό οι συμετέχοντες να βρουν και να εγκαταστήσουν ενα θέμα εμφάνισης στο δικό τους ιστότοπο</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Ασκήσεις</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><strong>Όνομα άσκησης</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Σύντομη περιγραφή της άσκηση και των δεξιότητων/γνώσεων που ενισχύει.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Σύντομο σημείο ή βήμα της άσκησης</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Ακόμα ένα</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Αυτές είναι σύντομες ή συγκεκριμένες δραστηριότητες που βοηθούν τους συμμετέχοντες να εξασκήσουν ορισμένα στοιχεία του μαθήματος. Δεν θα πρέπει να είναι ασκήσεις με πλήρες σενάριο, αλλά κάτι που οι συμμετέχοντες θα μπορούσαν να κάνουν μόνοι τους. Για παράδειγμα, μπορείτε να δημιουργήσετε μια άσκηση βασισμένη σε ένα βήμα του παραδείγματος μαθήματος.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Αξιολόγηση</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Θα πρέπει να υπάρχει ένα στοιχείο αξιολόγησης (ή περισσότερα) για κάθε στόχο που αναφέρεται παραπάνω. Κάθε στοιχείο αξιολόγησης πρέπει να υποστηρίζει έναν στόχο. δεν πρέπει να υπάρχει κανένα σημείο αξιολόγησης που να μην το κάνει.<p/>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Γράψτε την ερώτηση.</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"ordered":true} -->
+<ol>
+<!-- wp:list-item -->
+<li>Απάντηση</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Απάντηση</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Απάντηση</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Απάντηση</li>
+<!-- /wp:list-item -->
+</ol>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><strong>Απάντηση:</strong>&nbsp;3. Σωστή απάντηση</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Μερικές ερωτήσεις που πρέπει να κάνετε στους συμμετέχοντες για να αξιολογήσετε την κατανόηση του υλικού που παρουσιάζετε. Θα πρέπει να είναι ένα μέτρο για το εάν επιτεύχθηκαν οι στόχοι. Σκεφτείτε να έχετε μια ερώτηση για κάθε στόχο.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Επιπρόσθετοι πόροι</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Πόρος 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Πόρος 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Μια προαιρετική ενότητα που μπορεί να περιέχει μια λίστα πόρων που μπορεί να χρησιμοποιήσει ο παρουσιαστής για να λάβει περισσότερες πληροφορίες σχετικά με το θέμα.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Παράδειγμα:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Σύνδεσμος προς πληροφορίες σχετικά με τα <a data-external="true" href="https://wordpress.org/support/">Έγγραφα υποστήριξης</a> ή τους <a data-external="true" href="https: //developer.wordpress.org/">Πόρους προγραμματιστών</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a data-external="true" href="https://developer.wordpress.org/themes/">Εγχειρίδιο ομάδας ελέγχου θεμάτων εμφάνισης</a> ή <a href="https://make.wordpress.org/ plugins/handbook/">Εγχειρίδιο ομάδας ελέγχου πρόσθετων</a></li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Παράδειγμα:</h2>
+<!-- /wp:heading -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Ένα παράδειγμα για το πώς μπορεί να εφαρμοστεί το πλάνο μαθήματος. Γραπτό σε μορφή σεναρίου ως ένας πιθανός τρόπος με τον οποίο ένας παρουσιαστής μπορεί να χρησιμοποιήσει αυτό το πλάνο μαθήματος σε μια εκδήλωση, με στιγμιότυπα οθόνης και οδηγίες εάν είναι απαραίτητο.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"level":3} -->
+<h3>Παράδειγμα μαθήματος</h3>
+<!-- /wp:heading -->
+
+<!-- wp:quote {"className":"is-style-default"} -->
+<blockquote class="wp-block-quote is-style-default">
+<!-- wp:paragraph -->
+<p>Πιθανότατα θα χρειαστεί να χωρίσετε το πλάνο μαθήματος σε πολλές ενότητες.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"level":3,"lock":{"move":true,"remove":true}} -->
+<h3>Κλείσιμο μαθήματος</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>[tip] 💡 Ακολουθήστε τις <a href="#Exercises">Ασκήσεις</a> και την <a href="#Assessment">Αξιολόγηση</a> που περιγράφονται παραπάνω.[/tip]</p>
+<!-- /wp:paragraph -->
