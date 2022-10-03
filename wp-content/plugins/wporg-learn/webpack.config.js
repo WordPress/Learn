45d6e9b0a65e390efd56be3c8247620be3d39834
ren <18050944+renintw@users.noreply.github.com>
2022-10-04 00:25:56 +0800

    Update @wordpress/scripts

--- wp-content/plugins/wporg-learn/webpack.config.js
+++ wp-content/plugins/wporg-learn/webpack.config.js
@@ -8,22 +8,11 @@ config.entry = {
 	'expiration-date': './js/expiration-date/index.js',
 	'workshop-application-form': './js/workshop-application-form/src/index.js',
 	'workshop-details': './js/workshop-details/src/index.js',
-	'event': './js/event.js',
-	'form': './js/form.js',
+	event: './js/event.js',
+	form: './js/form.js',
 	'locale-notice': './js/locale-notice.js',
 	'lesson-plan-actions': './js/lesson-plan-actions/src/index.js',
 	'lesson-plan-details': './js/lesson-plan-details/src/index.js',
-}
-
-/**
- * The jsonpFunction is a global function used to load application chunks. If
- * multiple webpack-bundled scripts are on the same page, these functions will
- * conflict. This provides a unique name for this function in our app.
- *
- * @see https://github.com/WordPress/gutenberg/issues/23607
- * @see https://webpack.js.org/configuration/output/#outputjsonpfunction
- * @see https://github.com/WordPress/gutenberg/issues/24321
- */
-config.output.jsonpFunction = 'wporgLearnPlugin';
+};
 
 module.exports = config;
