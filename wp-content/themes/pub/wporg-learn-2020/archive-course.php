ab0a84050cbaa8ef6f5f8e7d9517381f4ebda870
Hugh Lashbrooke <1543143+hlashbrooke@users.noreply.github.com>
2022-11-29 12:22:08 +1300

    Adding description and links to course archive
    This PR adds a page description and anchor links to the course archive template. Fixes #1114.

--- wp-content/themes/pub/wporg-learn-2020/archive-course.php
+++ wp-content/themes/pub/wporg-learn-2020/archive-course.php
@@ -12,6 +12,8 @@ namespace WordPressdotorg\Theme;
 $prev_category = '';
 $category_title = '';
 $category_description = '';
+$courses_list = '';
+$course_categories = array();
 
 get_header();
 get_template_part( 'template-parts/component', 'breadcrumbs' );
@@ -37,34 +39,65 @@ get_template_part( 'template-parts/component', 'breadcrumbs' );
 
 					if ( isset( $categories[0] ) ) {
 						$category_title       = $categories[0]->name;
+						$category_slug        = $categories[0]->slug;
 						$category_description = $categories[0]->description;
 						$begin_new_category   = $category_title !== $prev_category;
 					}
 
 					if ( $begin_new_category ) {
+
+						$course_categories[ $category_slug ] = $category_title;
+
 						// Close the previous card-grid if there was a previous category
 						if ( ! empty( $prev_category ) ) {
-							echo '</div>';
+							$courses_list .= '</div>';
 						}
-						?>
-		<h2 class="h4 course-category-header"><?php echo esc_html( $category_title ); ?></h2>
-						<?php
+						$courses_list .= '<h2 class="h4 course-category-header" id="' . esc_attr( $category_slug ) . '">' . esc_html( $category_title ) . '</h2>';
+
 						if ( $category_description ) {
-							echo '<div class="course-category-description">' . esc_html( $category_description ) . '</div>';
+							$courses_list .=  '<div class="course-category-description">' . esc_html( $category_description ) . '</div>';
 						}
-						?>
-		<div class="card-grid card-grid_2">
-						<?php
+						$courses_list .= '<div class="card-grid card-grid_2">';
 						$prev_category = $category_title;
 					}
 
+					ob_start();
 					get_template_part(
 						'template-parts/component',
 						'card',
 						wporg_learn_get_card_template_args( get_the_ID() )
 					);
+					$courses_list .= ob_get_clean();
+
 				endwhile; ?>
-		</div>
+
+				<?php
+				$courses_list .= '</div>';
+
+				if( ! empty( $course_categories ) ) { ?>
+					<p>
+						<br>
+						<?php _e( 'WordPress provides limitless ways for people to craft and grow their online presence. The content in these courses is delivered in multiple formats, with a focus on text and video, working towards practical learning objectives to help you become a better WordPress developer, designer, user, and contributor.', 'wporg-learn' );
+						?>
+					</p>
+					<p>
+						<?php
+						$i = 0;
+						foreach ( $course_categories as $slug => $title ) {
+							if ( 0 < $i ) { echo ' | '; }
+							echo '<a href="#' . esc_attr( $slug ) . '">' . esc_html( $title ) . '</a>';
+							$i++;
+						}
+						?>
+					</p>
+
+					<hr>
+				<?php }
+
+				echo $courses_list;
+
+				?>
+
 			<?php else : ?>
 				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 			<?php endif; ?>
