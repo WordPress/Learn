1ef2282c75f5f0d80badf34a5c47740e034d2a8a
Hugh Lashbrooke <1543143+hlashbrooke@users.noreply.github.com>
2022-12-02 16:03:41 +1300

    Update wp-content/themes/pub/wporg-learn-2020/page-online-workshops.php
    Co-authored-by: Adam Wood <adam.wood@automattic.com>

--- wp-content/themes/pub/wporg-learn-2020/page-online-workshops.php
+++ wp-content/themes/pub/wporg-learn-2020/page-online-workshops.php
@@ -26,12 +26,8 @@ get_template_part( 'template-parts/component', 'breadcrumbs' );
 				<div class="row between gutters">
 					<p class="col-8"><?php esc_html_e( 'Online workshops are live sessions where you can learn alongside other WordPress enthusiasts. They are a safe zone where you can come as you are, develop new ideas, explore issues, ask questions, network over shared interests, exchange theories, collaborate on work, and thrive in uncertainty.', 'wporg-learn' ); ?></p>
 					<div class="col-4">
-						<div class="wp-block-button is-style-secondary-full-width">
-							<a class="wp-block-button__link" href="#apply-to-facilitate" style="border-radius:5px"><?php esc_html_e( 'Apply to facilitate', 'wporg-learn' ); ?></a>
-						</div>
-						<div class="wp-block-button is-style-secondary-full-width">
-							<a class="wp-block-button__link" href="https://wordpress.tv/category/learn-wordpress-online-workshops/" target="_blank" style="border-radius:5px"><?php esc_html_e( 'View recorded online workshops', 'wporg-learn' ); ?></a>
-						</div>
+						<a class="button button-xlarge button-secondary" href="#apply-to-facilitate" style="border-radius:5px"><?php esc_html_e( 'Apply to facilitate', 'wporg-learn' ); ?></a>
+						<a class="button button-xlarge button-secondary" href="https://wordpress.tv/category/learn-wordpress-online-workshops/" target="_blank" style="border-radius:5px"><?php esc_html_e( 'View recorded online workshops', 'wporg-learn' ); ?></a>
 					</div>
 				</div>
 			</div>
