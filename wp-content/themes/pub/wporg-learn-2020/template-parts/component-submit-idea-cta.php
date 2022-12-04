3e48978fb7bf6dccdff203872e92158fed9efcf0
Adam Wood <adam.wood@automattic.com>
2022-12-05 12:06:45 +1300

    Use flex for responsive button layout

--- wp-content/themes/pub/wporg-learn-2020/template-parts/component-submit-idea-cta.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-submit-idea-cta.php
@@ -16,10 +16,12 @@ $args = wp_parse_args( $args );
 		<div aria-hidden="true" class="content-icon"><span class="dashicons dashicons-<?php echo esc_attr( $args['icon'] ); ?>"></span></div>
 	<?php endif; ?>
 	<h2><?php esc_html_e( 'Have an idea for new content? Let us know!', 'wporg-learn' ); ?></h2>
-	<a class="button button-primary button-large" href="https://learn.wordpress.org/tutorial-presenter-application/">
-		<?php esc_html_e( 'Apply to present a tutorial', 'wporg-learn' ); ?>
-	</a>
-	<a class="button button-secondary button-large" href="https://github.com/WordPress/Learn/issues/new?assignees=&labels=Awaiting+Triage%2C+Needs+Subject+Matter+Expert&template=topic-idea.md&title=Topic+Idea%3A+TOPIC+TITLE" target="_blank" rel="noreferrer noopener">
-		<?php esc_html_e( 'Submit a topic idea', 'wporg-learn' ); ?>
-	</a>
+	<div class="buttons">
+		<a class="button button-primary button-large" href="https://learn.wordpress.org/tutorial-presenter-application/">
+			<?php esc_html_e( 'Apply to present a tutorial', 'wporg-learn' ); ?>
+		</a>
+		<a class="button button-secondary button-large" href="https://github.com/WordPress/Learn/issues/new?assignees=&labels=Awaiting+Triage%2C+Needs+Subject+Matter+Expert&template=topic-idea.md&title=Topic+Idea%3A+TOPIC+TITLE" target="_blank" rel="noreferrer noopener">
+			<?php esc_html_e( 'Submit a topic idea', 'wporg-learn' ); ?>
+		</a>
+	</div>
 </section>
