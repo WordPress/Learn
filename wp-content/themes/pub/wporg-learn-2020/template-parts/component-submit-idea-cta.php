f733fc33072b0fbc3cb1d51dd924b207389b545c
Hugh Lashbrooke <1543143+hlashbrooke@users.noreply.github.com>
2022-12-06 08:54:58 +1300

    Updates wording and buttons for CTA (#1150)
    * Updates wording and buttons for CTA
    
    Fixes #1149
    
    * Add space between submit idea cta buttons
    
    * Stop submit idea page styles applying to other pages
    
    * Rebuild styles
    
    * Remove duplicate screen reader only content
    
    * Use flex for responsive button layout
    
    Co-authored-by: Adam Wood <adam.wood@automattic.com>

--- wp-content/themes/pub/wporg-learn-2020/template-parts/component-submit-idea-cta.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/component-submit-idea-cta.php
@@ -15,6 +15,13 @@ $args = wp_parse_args( $args );
 	<?php if ( isset( $args['icon'] ) ) : ?>
 		<div aria-hidden="true" class="content-icon"><span class="dashicons dashicons-<?php echo esc_attr( $args['icon'] ); ?>"></span></div>
 	<?php endif; ?>
-	<h2><?php esc_html_e( 'Have an Idea for a Tutorial? Let us know!', 'wporg-learn' ); ?></h2>
-	<a class="button button-primary button-large" href="https://learn.wordpress.org/tutorial-presenter-application/"><span aria-hidden="true"><?php esc_html_e( 'Submit an Idea', 'wporg-learn' ); ?></span><span class="screen-reader-text"><?php esc_html_e( 'Submit Tutorial Idea', 'wporg-learn' ); ?></span></a>
+	<h2><?php esc_html_e( 'Have an idea for new content? Let us know!', 'wporg-learn' ); ?></h2>
+	<div class="buttons">
+		<a class="button button-primary button-large" href="https://learn.wordpress.org/tutorial-presenter-application/">
+			<?php esc_html_e( 'Apply to present a tutorial', 'wporg-learn' ); ?>
+		</a>
+		<a class="button button-secondary button-large" href="https://github.com/WordPress/Learn/issues/new?assignees=&labels=Awaiting+Triage%2C+Needs+Subject+Matter+Expert&template=topic-idea.md&title=Topic+Idea%3A+TOPIC+TITLE" target="_blank" rel="noreferrer noopener">
+			<?php esc_html_e( 'Submit a topic idea', 'wporg-learn' ); ?>
+		</a>
+	</div>
 </section>
