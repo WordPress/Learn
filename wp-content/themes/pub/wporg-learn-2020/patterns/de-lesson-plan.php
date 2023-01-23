b4f62922a5e6a5856ed1e087869b254c46ed10ea
Ben Evans <49054298+bsanevans@users.noreply.github.com>
2023-01-23 11:49:41 +0900

    German lesson plan pattern translation (#1180)
    * German lesson plan pattern translation
    
    This is a German translation of the lesson plan pattern, provided by @gudrunfrank.
    
    A translation review has been completed and is now ready to be merged to the site.

---
+++ wp-content/themes/pub/wporg-learn-2020/patterns/de-lesson-plan.php
@@ -0,0 +1,391 @@
+<?php
+/**
+ * Title: German Lesson Plan Content
+ * Slug: wporg-learn-2020/lesson-plan/german
+ * Block Types: core/post-content
+ * Post Types: lesson-plan
+ * Categories: featured, text
+ */
+
+?>
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Ein kurzer Absatz über die Lektion. Er sollte so geschrieben sein, dass der Text für ein Meetup oder Workshop als Beschreibung verwendet werden kann.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Lernziele</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Nach dieser Lektion können Teilnehmende:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Lernziel 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Lernziel 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Jede Lektion muss eine Liste von Lernzielen enthalten.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Lernziele sollten als Aktionen formuliert werden, die die Teilnehmenden nach der Lektion ausführen können. Siehe <a href="https://wb-web.de/material/methoden/lernziele-formulieren-leicht-gemacht.html">Lernziele formulieren leicht gemacht<a/> und <a href=https://wb-web.de/material/methoden/formulierungshilfen-fur-lernziele.html>Formulierungshilfen für Lernziele</a>. Begriffe wie „wissen", „verstehen", „lernen", „sich bewusst sein" usw. sollten möglichst vermieden werden. Jedes Lernziel sollte mit einer Übung (siehe unten) überprüft werden.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Vorrausgesetzte Fähigkeiten</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Folgende Fähigkeiten helfen dir, dieser Lektion zu folgen:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Fähigkeit 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Fähigkeit 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><em>Zum Beispiel:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Erfahrung mit HTML und CSS</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Die Lektion „Dashboard Übersicht" abgeschlossen</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Screening-Fragen</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Frage 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Frage 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Dies ist eine Liste von Fragen, die überprüfen soll, ob die Lernenden die Voraussetzungen mitbringen, dieser Lektion zu folgen.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Zum Beispiel:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Willst du Veränderungen am Theme selbst vornehmen?</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Weißt du, wie man CSS schreibt?</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Du brauchst:</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Listenpunkt 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Listenpunkt 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Eine Liste mit Dateien, Ressourcen, Geräten oder anderen Materialien, die der Moderator für die Lektion benötigt.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Zum Beipiel:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Eine lokale Installation von WordPress</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Die Dateien für das Theme Twenty Sixteen</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Anmerkungen für Moderatoren</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Notiz 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Notiz 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Eine Liste von Tipps und Notizen für den Moderator.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Zum Beispiel:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Teilnehmende sollten das Theme Twenty Sixteen herunterladen, bevor sie anfangen</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Hinweise für den Fall, dass kein Projektor oder Internet zur Verfügung steht.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Tipps für den Fall, dass jemand nicht die nötigen Materialien zur Verfügung hat.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Wie man mit verschiedenen Meinungen zu diesem Thema umgehen kann.</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Lektions-Übersicht</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Zuerst dies</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Dann das</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Zuletzt dies</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Ein Entwurf der Lektion, zB. im Gliederungsformat. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Zum Beispiel:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Erkläre, was ein Theme ist</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Demonstriere, wie ein Theme heruntergeladen und aktiviert wird</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Lernende üben selbst, ein Theme herunterzuladen und zu installieren.</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Übungen</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><strong>Name der Übung</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Kurze Beschreibung der Übung und welche Fähigkeiten sie vertiefen soll.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Übung 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Übung 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Dies sind kurze oder spezifische Aktivitäten, die den Teilnehmenden helfen, bestimmte Bestandteile der Lektion zu üben. Es sollten keine vollständig geskripteten Übungen sein, sondern etwas, das die Teilnehmenden selbst durchführen können. Dies kann eine Übung, basierend auf einem Schritt der Lektion, sein.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Quiz</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Für jedes oben aufgeführte Lernziel sollte es eine (oder mehrere) Fragen geben. Jede Frage sollte ein Lernziel unterstützen; es sollte keine Fragen geben, die das nicht tun.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Schreibe die Fragen.</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"ordered":true} -->
+<ol>
+<!-- wp:list-item -->
+<li>Antwort</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Antwort</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Antwort</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Antwort</li>
+<!-- /wp:list-item -->
+</ol>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><strong>Option:</strong>&nbsp;3. Richtige Antwort</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Ein paar Fragen, die den Teilnehmenden gestellt werden sollten, um zu bewerten, wie sie das präsentierte Material behalten haben. Sie sollten ein Maßstab dafür sein, ob die Ziele erreicht wurden. Jedes Lernziel sollte mit einer Frage überprüft werden.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Zusätzliche Ressourcen</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Ressource 1</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Ressource 2</li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Hier können zusätzliche Ressourcen genannt werden, auf die der Moderator zugreifen kann.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Zum Beispiel:</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<!-- wp:list-item -->
+<li>Link zu <a data-external="true" href="https://wordpress.org/support/">Support Docs</a> oder <a data-external="true" href="https://developer.wordpress.org/">Developer Resources (engl.)</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a data-external="true" href="https://developer.wordpress.org/themes/">Theme Review Team's Handbook (engl.)</a> oder <a href="https://make.wordpress.org/plugins/handbook/">Plugin Review Team's Handbook (engl.)</a></li>
+<!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:heading {"lock":{"move":true,"remove":true}} -->
+<h2>Beispiel</h2>
+<!-- /wp:heading -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote">
+<!-- wp:paragraph -->
+<p>Ein Beispiel, wie der Unterrichtsplan umgesetzt werden kann. Geschrieben in Skriptform als eine Möglichkeit, wie ein Moderator diesen Unterrichtsplan bei einer Veranstaltung verwenden könnte. Mit Screenshots und Anweisungen, falls erforderlich.</p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"level":3} -->
+<h3>Überschrift der Beispiel-Lektion</h3>
+<!-- /wp:heading -->
+
+<!-- wp:quote {"className":"is-style-default"} -->
+<blockquote class="wp-block-quote is-style-default">
+<!-- wp:paragraph -->
+<p>Die Beispiel-Lektion muss möglicherweise in Abschnitte unterteilt werden. </p>
+<!-- /wp:paragraph -->
+</blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:heading {"level":3,"lock":{"move":true,"remove":true}} -->
+<h3>Ausblick</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>[tip] 💡 Führe abschließend die <a href="#Exercises">Übungen</a> und das <a href="#Assessment">Quiz</a> wie oben angegeben durch.[/tip]</p>
+<!-- /wp:paragraph -->
+
