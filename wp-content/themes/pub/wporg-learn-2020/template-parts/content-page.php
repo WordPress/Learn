2667afe22e344e7ffa402e57a8d25a29bfbe6289
Shekh Aezaz <aezazahemad.shekh@multidots.com>
2023-02-21 19:22:03 +0530

    Remove the white space above the footer.

--- wp-content/themes/pub/wporg-learn-2020/template-parts/content-page.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/content-page.php
@@ -22,18 +22,4 @@
 			?>
 		</section>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php
-		edit_post_link(
-			sprintf(
-			/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', 'wporg-learn' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
-		?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
