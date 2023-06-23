1f0d0f9f14c24d73316f43c395bc1e95cd5974cb
Alex Stine <alex.stine@yourtechadvisors.com>
2023-06-22 21:29:08 -0500

    Replace section tags with div tags. (#1644)

--- wp-content/themes/pub/wporg-learn-2020/template-parts/content-workshop-single.php
+++ wp-content/themes/pub/wporg-learn-2020/template-parts/content-workshop-single.php
@@ -19,7 +19,7 @@ $other_contributors = array_map(
 );
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<section>
+	<div>
 		<div class="row align-middle between section-heading section-heading--with-space">
 			<h1 class="section-heading_title h2"><?php the_title(); ?></h1>
 		</div>
@@ -98,5 +98,5 @@ $other_contributors = array_map(
 				</section>
 			<?php endif; ?>
 		</div> <!-- end workshop-page -->
-	</section>
+	</div>
 </article>
