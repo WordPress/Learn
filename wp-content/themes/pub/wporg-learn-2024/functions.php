9d001201b08a35f8b9280f235572b9a20f7dc325
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-04-23 19:52:52 +1200

    Add 2024 theme (+1 squashed commit) Squashed commits: [025d16d] Fix stylelint errors

---
+++ wp-content/themes/pub/wporg-learn-2024/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WordPressdotorg\Theme\Learn_2024;
+
+/**
+ * Actions and filters.
+ */
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function enqueue_assets() {
+	// The parent style is registered as `wporg-parent-2021-style`, and will be loaded unless
+	// explicitly unregistered. We can load any child-theme overrides by declaring the parent
+	// stylesheet as a dependency.
+	wp_enqueue_style(
+		'wporg-learn-2024-style',
+		get_stylesheet_directory_uri() . '/build/style/style-index.css',
+		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
+		filemtime( __DIR__ . '/build/style/style-index.css' )
+	);
+}
