3849564f0b6712cbf7f205cfecb0abe90dc40f04
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-04-24 11:05:29 +1200

    Update dependencies to build on Node 20 (#2391)
    * Update dependencies to build on Node 20
    
    * Remove duplicate wp-scripts dependency
    
    * Fix lint errors
    
    * Fix stylelint errors
    
    * Try using v4 github actions

--- wp-content/plugins/wporg-learn/js/hooks/use-is-block-in-sidebar.js
+++ wp-content/plugins/wporg-learn/js/hooks/use-is-block-in-sidebar.js
@@ -5,9 +5,8 @@ import { useSelect } from '@wordpress/data';
 
 export const useIsBlockInSidebar = ( clientId, sidebarName ) => {
 	return useSelect( ( select ) => {
-		const { getBlockAttributes, getBlockName, getBlockParents } = select(
-			'core/block-editor'
-		);
+		const { getBlockAttributes, getBlockName, getBlockParents } =
+			select( 'core/block-editor' );
 		const parents = getBlockParents( clientId );
 		return parents.some( ( parent ) => {
 			if ( 'core/widget-area' !== getBlockName( parent ) ) {
