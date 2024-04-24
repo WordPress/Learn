3d10f1ba199af9c7624460ede628fac2187fc272
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-04-24 13:43:07 +1200

    Add local nav items

--- wp-content/themes/pub/wporg-learn-2024/functions.php
+++ wp-content/themes/pub/wporg-learn-2024/functions.php
@@ -6,6 +6,7 @@ namespace WordPressdotorg\Theme\Learn_2024;
  * Actions and filters.
  */
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+add_filter( 'wporg_block_navigation_menus', __NAMESPACE__ . '\add_site_navigation_menus' );
 
 /**
  * Enqueue scripts and styles.
@@ -21,3 +22,34 @@ function enqueue_assets() {
 		filemtime( __DIR__ . '/build/style/style-index.css' )
 	);
 }
+
+/**
+ * Provide a list of local navigation menus.
+ */
+function add_site_navigation_menus( $menus ) {
+	return array(
+		'learn' => array(
+			array(
+				'label' => __( 'Courses', 'wporg-learn' ),
+				'url' => '/courses/',
+			),
+			array(
+				'label' => __( 'Lessons', 'wporg-learn' ),
+				'url' => '/lessons/',
+			),
+			array(
+				'label' => __( 'Learning Pathways', 'wporg-learn' ),
+				'url' => '/learning-pathways/',
+			),
+			array(
+				'label' => __( 'Contribute', 'wporg-learn' ),
+				'url' => '/contribute/',
+			),
+			array(
+				'label' => __( 'Instruct', 'wporg-learn' ),
+				'url' => '/instruct/',
+			),
+		),
+	);
+}
+
