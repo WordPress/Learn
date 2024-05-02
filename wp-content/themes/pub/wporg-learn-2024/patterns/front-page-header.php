15db4b84d41602cf38ecfbea8153356f44c5b58f
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-05-03 09:45:50 +1200

    Add front page header and content patterns
    See https://github.com/WordPress/Learn/issues/2394

---
+++ wp-content/themes/pub/wporg-learn-2024/patterns/front-page-header.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Title: Front Page Header
+ * Slug: wporg-learn-2024/front-page-header
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:wporg/global-header {"style":{"border":{"bottom":{"color":"var:preset|color|white-opacity-15","style":"solid","width":"1px"}}}} /-->
+
+<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}}},"textColor":"white","fontSize":"small"} -->
+
+	<!-- wp:site-title {"level":0,"fontSize":"small","className":"wporg-local-navigation-bar__show-on-scroll"} /-->
+
+	<!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"charcoal-2","overlayTextColor":"white","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","menuSlug":"learn"} /-->
+
+<!-- /wp:wporg/local-navigation-bar -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-2","className":"has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)">
+
+	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"bottom"}} -->
+	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--30)">
+	
+		<!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"50px","fontStyle":"normal","fontWeight":"400"}},"fontFamily":"eb-garamond"} -->
+		<h1 class="wp-block-heading has-eb-garamond-font-family" style="font-size:50px;font-style:normal;font-weight:400"><?php esc_html_e( 'Learn WordPress', 'wporg' ); ?></h1>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"style":{"typography":{"lineHeight":"2.3"}},"textColor":"white"} -->
+		<p class="has-white-color has-text-color" style="line-height:2.3"><?php esc_html_e( 'Your free educational resource for how to use, extend, and build with WordPress', 'wporg-learn' ); ?></p>
+		<!-- /wp:paragraph -->
+	
+	</div>
+	<!-- /wp:group -->
+
+	<!-- wp:pattern {"slug":"wporg-learn-2024/search-field"} /-->
+
+</div>
+<!-- /wp:group -->
