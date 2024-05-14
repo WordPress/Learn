73c8d2db6c02661e717914607053a3920c4b8683
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-05-15 11:39:22 +1200

    Add no results handling to Course templates

---
+++ wp-content/themes/pub/wporg-learn-2024/patterns/query-no-results-course.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Query no Courses found
+ * Slug: wporg-learn-2024/query-no-results-course
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:query-no-results -->
+
+	<!-- wp:paragraph -->
+	<p><?php echo esc_html__( 'No Courses found.', 'wporg-learn' ); ?></p>
+	<!-- /wp:paragraph -->
+
+<!-- /wp:query-no-results -->
