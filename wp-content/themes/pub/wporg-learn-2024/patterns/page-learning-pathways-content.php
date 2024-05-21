22859d8a84ce949f99ba3edfa9d2de659027f0fb
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-05-22 10:41:09 +1200

    Update Learning Pathways page

---
+++ wp-content/themes/pub/wporg-learn-2024/patterns/page-learning-pathways-content.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Learning Pathways Page Content
+ * Slug: wporg-learn-2024/page-learning-pathways-content
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<p style="margin-top:0"><?php esc_html_e( 'Dive into our learning pathways which will take you from beginner to expert at your own pace.', 'wporg-learn' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-buttons" style="margin-bottom:var(--wp--preset--spacing--50)">
+
+	<!-- wp:button -->
+	<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/my-courses/"><?php esc_html_e( 'My Courses', 'wporg-learn' ); ?></a>
+	</div>
+	<!-- /wp:button -->
+
+</div>
+<!-- /wp:buttons -->
