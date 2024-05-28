f29130a962bfe72be25ed43bd98803a08a2415bb
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-05-28 15:11:40 +1200

    Move homepage footer content to main footer

---
+++ wp-content/themes/pub/wporg-learn-2024/patterns/footer-content.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Title: Footer Content
+ * Slug: wporg-learn-2024/footer-content
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"border":{"bottom":{"color":"var:preset|color|white-opacity-15","width":"1px"}},"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-charcoal-2-background-color has-background" style="border-bottom-color:var(--wp--preset--color--white-opacity-15);border-bottom-width:1px;padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)">
+
+	<!-- wp:columns {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"blockGap":{"left":"0"}}},"textColor":"white"} -->
+	<div class="wp-block-columns has-white-color has-text-color has-link-color">
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|edge-space"}}}} -->
+		<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--50)">
+
+			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"top":"0"}}},"fontSize":"huge","fontFamily":"eb-garamond"} -->
+			<h2 class="wp-block-heading has-eb-garamond-font-family has-huge-font-size" style="margin-top:0;font-style:normal;font-weight:400">TBD heading</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph {"fontSize":"large"} -->
+			<p class="has-large-font-size"><a href="https://wordpress.org/documentation"><?php esc_html_e( 'Documentation', 'wporg-learn' ); ?></a></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"fontSize":"large"} -->
+			<p class="has-large-font-size"><a href="https://developer.wordpress.org"><?php esc_html_e( 'Developer Resources', 'wporg-learn' ); ?></a></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"fontSize":"large"} -->
+			<p class="has-large-font-size"><a href="https://wordpress.org/support/forums"><?php esc_html_e( 'Support Forums', 'wporg-learn' ); ?></a></p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|edge-space","right":"0"}},"border":{"left":{"color":"var:preset|color|white-opacity-15","width":"1px"},"top":{},"right":{},"bottom":{}}},"className":"wporg-learn-footer-signup"} -->
+		<div class="wp-block-column wporg-learn-footer-signup" style="border-left-color:var(--wp--preset--color--white-opacity-15);border-left-width:1px;padding-top:var(--wp--preset--spacing--50);padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--edge-space)">
+
+			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"top":"0"}}},"fontSize":"huge","fontFamily":"eb-garamond"} -->
+			<h2 class="wp-block-heading has-eb-garamond-font-family has-huge-font-size" style="margin-top:0;font-style:normal;font-weight:400">TBD heading</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php esc_html_e( 'Sign up for the free Learn WordPress newsletter and get a monthly dose of what\'s new in the world of WordPress courses, lessons, and online workshops.', 'wporg-learn' ); ?></p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:column -->
+
+	</div>
+	<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
