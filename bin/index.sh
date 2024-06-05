d7e978159d7a8830ae9a67dcefad5f9774efa5f7
Bruno Freiberger <freibergergarcia@gmail.com>
2024-06-05 09:16:53 +0200

    Add wporg-learn plugin to index.sh
    #2495 add wporg-learn to the list of plugins that need to be activated during the environment setup

--- bin/index.sh
+++ bin/index.sh
@@ -15,7 +15,7 @@ npm run wp-env run cli wp db import wp-content/uploads/wporg_events.sql
 npm run wp-env run cli wp db import wp-content/uploads/wporg_locales.sql
 
 # Activate plugins
-npm run wp-env run cli wp plugin activate edit-flow jetpack wordpress-importer sensei-lms gutenberg locale-detection code-syntax-block
+npm run wp-env run cli wp plugin activate edit-flow jetpack wordpress-importer sensei-lms gutenberg locale-detection code-syntax-block wporg-learn
 
 # Activate jetpack modules
 npm run wp-env run cli wp jetpack module activate contact-form
