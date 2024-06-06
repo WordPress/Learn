380e12c6c46bf4bc23738c17d6c87f186852c504
Bruno FG <freibergergarcia@gmail.com>
2024-06-06 17:26:05 +1000

    Activate wporg-learn plugin at env setup (#2503)
    * Add wporg-learn plugin to index.sh
    
    #2495 add wporg-learn to the list of plugins that need to be activated during the environment setup
    
    * Fix import content command
    
    #2495 remove double quotes from the import content command
    
    * Update build process and uncomment content import
    
    Covers #2495

--- bin/index.sh
+++ bin/index.sh
@@ -15,7 +15,7 @@ npm run wp-env run cli wp db import wp-content/uploads/wporg_events.sql
 npm run wp-env run cli wp db import wp-content/uploads/wporg_locales.sql
 
 # Activate plugins
-npm run wp-env run cli wp plugin activate edit-flow jetpack wordpress-importer sensei-lms gutenberg locale-detection code-syntax-block
+npm run wp-env run cli wp plugin activate edit-flow jetpack wordpress-importer sensei-lms gutenberg locale-detection code-syntax-block wporg-learn
 
 # Activate jetpack modules
 npm run wp-env run cli wp jetpack module activate contact-form
@@ -27,4 +27,4 @@ npm run wp-env run cli wp theme activate pub/wporg-learn-2024
 npm run wp-env run cli wp rewrite structure '/%postname%/'
 
 # Import content
-# npm run wp-env run cli "php bin/import-test-content.php"
+npm run wp-env run cli php bin/import-test-content.php
