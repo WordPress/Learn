3554680c5f1daf9cad533d9c8b4e7ae5916835f1
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-06-07 17:14:02 +1200

    Add a custom template for the Lesson Plan archive
    Includes intro from the current site

--- wp-content/themes/pub/wporg-learn-2024/patterns/archive-lesson-plan-content.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/archive-lesson-plan-content.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Archive Content
- * Slug: wporg-learn-2024/archive-content
+ * Title: Lesson Plan Archive Content
+ * Slug: wporg-learn-2024/archive-lesson-plan-content
  * Inserter: no
  */
 
@@ -12,13 +12,17 @@
 
 	<!-- wp:query-title {"type":"archive","showPrefix":false} /-->
 
+	<!-- wp:paragraph -->
+	<p><?php esc_html_e( 'Want to help others learn about WordPress? Read through, use, and remix these lesson plans.', 'wporg-learn' ); ?></p>
+	<!-- /wp:paragraph -->
+
 </div>
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|50"}}}} -->
 <div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--50)">
 
-	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","width":290,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"query":{"post_type":"<?php echo esc_attr( get_post_type() ); ?>"}} /-->
+	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search lesson plans', 'wporg-learn' ); ?>","width":290,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"query":{"post_type":"<?php echo esc_attr( get_post_type() ); ?>"}} /-->
 
 	<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"},"className":"wporg-query-filters"} -->
 	<div class="wp-block-group wporg-query-filters">
@@ -31,7 +35,7 @@
 </div>
 <!-- /wp:group -->
 
-<!-- wp:query {"queryId":1,"query":{"perPage":12,"pages":0,"offset":0,"postType":"","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"parents":[]}} -->
+<!-- wp:query {"queryId":1,"query":{"perPage":12,"pages":0,"offset":0,"postType":"lesson-plan","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"parents":[]}} -->
 <div class="wp-block-query">
 
 	<!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"330px"}} -->
@@ -65,7 +69,7 @@
 	<!-- wp:query-no-results -->
 
 		<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-		<p><?php esc_html_e( 'Nothing found.', 'wporg-learn' ); ?></p>
+		<p><?php esc_html_e( 'No lesson plans found.', 'wporg-learn' ); ?></p>
 		<!-- /wp:paragraph -->
 
 	<!-- /wp:query-no-results -->
