287723756a640e0ee79cd759e0d1aa898e9b96a0
ren <18050944+renintw@users.noreply.github.com>
2024-06-10 07:09:54 +0900

    Add course-progress to sidebar.

--- wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-meta-info.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-meta-info.php
@@ -10,15 +10,17 @@
 <!-- wp:group {"align":"full","className":"wporg-learn-sidebar-meta-info","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull wporg-learn-sidebar-meta-info">
 
+	<!-- wp:sensei-lms/course-progress {"defaultBarColor":"blueberry-1"} /-->
+
 	<!-- wp:sensei-lms/button-take-course {"align":"full","borderRadius":2,"className":"is-style-default"} -->
 	<div class="wp-block-sensei-lms-button-take-course is-style-default wp-block-sensei-button wp-block-button has-text-align-full">
 	<button class="wp-block-button__link" style="border-radius:2px">Take this Course</button>
 	</div>
 	<!-- /wp:sensei-lms/button-take-course -->
 
-	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"className":""wporg-learn-sidebar-playground"} -->
 	<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","width":100,"style":{"border":{"radius":"2px"},"spacing":{"padding":{"left":"13px","right":"13px","top":"16px","bottom":"16px"}},"typography":{"lineHeight":0,"fontStyle":"normal","fontWeight":"400"}},"className":"aligncenter is-style-outline","fontSize":"normal","fontFamily":"inter"} -->
-	<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size aligncenter is-style-outline has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400;line-height:0"><a class="wp-block-button__link has-text-align-center wp-element-button" href="https://wordpress.org/playground/demo/?step=playground&amp;theme=twentytwentythree" style="border-radius:2px;padding-top:16px;padding-right:13px;padding-bottom:16px;padding-left:13px" target="_blank" rel="noreferrer noopener">Practice on a private demo site</a></div>
+	<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size aligncenter is-style-outline has-inter-font-family has-normal-font-size wporg-learn-sidebar-playground" style="font-style:normal;font-weight:400;line-height:0"><a class="wp-block-button__link has-text-align-center wp-element-button" href="https://wordpress.org/playground/demo/?step=playground&amp;theme=twentytwentythree" style="border-radius:2px;padding-top:16px;padding-right:13px;padding-bottom:16px;padding-left:13px" target="_blank" rel="noreferrer noopener">Practice on a private demo site</a></div>
 	<!-- /wp:button --></div>
 	<!-- /wp:buttons -->
 
