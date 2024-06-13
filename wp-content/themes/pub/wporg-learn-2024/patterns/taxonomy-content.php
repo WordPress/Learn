715f50db662be67fc9f62af3abd60fb96d5d9b65
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-06-13 14:55:56 +1200

    Extract learning pathway header into a block for reuse

--- wp-content/themes/pub/wporg-learn-2024/patterns/taxonomy-content.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/taxonomy-content.php
@@ -12,75 +12,43 @@ if ( ! is_tax() ) {
 global $wp_query;
 
 if ( isset( $wp_query->query_vars['wporg_learning_pathway'] ) ) {
-
-	$learning_pathway_object = get_term_by( 'slug', $wp_query->query_vars['wporg_learning_pathway'], 'learning-pathway' );
-
-	$learning_pathway_id = $learning_pathway_object->term_id;
-	$learning_pathway_slug = $learning_pathway_object->slug;
-	$learning_pathway_name = $learning_pathway_object->name;
-	$learning_pathway_description = $learning_pathway_object->description;
+	$learning_pathway_slug = $wp_query->query_vars['wporg_learning_pathway'];
 	?>
 
-	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"0","bottom":"0"}},"border":{"bottom":{"color":"var:preset|color|light-grey-1","width":"1px"}}},"backgroundColor":"<?php echo esc_attr( $learning_pathway_slug ); ?>","className":"wporg-learn-tax-learning-pathway-header","layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignfull has-<?php echo esc_attr( $learning_pathway_slug ); ?>-background-color has-background wporg-learn-tax-learning-pathway-header" style="border-bottom-color:var(--wp--preset--color--light-grey-1);border-bottom-width:1px;padding-top:0;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:0;padding-left:var(--wp--preset--spacing--edge-space)">
+	<!-- wp:wporg-learn/learning-pathway-header {"align":"full","learningPathwaySlug":"<?php echo esc_attr( $learning_pathway_slug ); ?>"} /-->
 
-		<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"background":{"backgroundRepeat":"no-repeat","backgroundSize":"contain","backgroundPosition":"100% 50%"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"stretch"}} -->
-		<div class="wp-block-group">
-			
-			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
-			<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+<?php } ?>
 
-				<!-- wp:heading {"level":1,"fontSize":"heading-1","fontFamily":"eb-garamond"} -->
-				<h1 class="wp-block-heading has-eb-garamond-font-family has-heading-1-font-size"><?php echo esc_html( $learning_pathway_name ); ?></h1>
-				<!-- /wp:heading -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60)">
 
-				<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}}} -->
-				<p style="line-height:1.6"><?php echo esc_html( $learning_pathway_description ); ?></p>
-				<!-- /wp:paragraph -->
+	<?php if ( ! $learning_pathway_slug ) { ?>
 
+		<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained","justifyContent":"left","contentSize":"730px"}} -->
+		<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--40)">
+
+			<!-- wp:query-title {"type":"archive","showPrefix":false} /-->
+
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+		<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
+
+			<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","width":290,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true} /-->
+
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"},"className":"wporg-query-filters"} -->
+			<div class="wp-block-group wporg-query-filters">
+				<!-- wp:wporg/query-filter {"key":"language"} /-->
+				<!-- wp:wporg/query-filter {"key":"topic"} /-->
+				<!-- wp:wporg/query-filter {"key":"level"} /-->
 			</div>
-			<!-- /wp:group -->
-
-			<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"33%"},"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/learning-pathway-' . $learning_pathway_slug . '.svg' ); ?>","source":"file"},"backgroundPosition":"0% 50%"}},"layout":{"type":"constrained"}} -->
-			<div class="wp-block-group"></div>
 			<!-- /wp:group -->
 
 		</div>
 		<!-- /wp:group -->
 
-	</div>
-	<!-- /wp:group -->
-
-<?php } else { ?>
-
-	<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained","justifyContent":"left","contentSize":"730px"}} -->
-	<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--40)">
-
-		<!-- wp:query-title {"type":"archive","showPrefix":false} /-->
-
-	</div>
-	<!-- /wp:group -->
-
-<?php } ?>
-
-<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
-<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-
-	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","width":290,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"query":{"wporg_learning_pathway":"<?php echo esc_attr( $learning_pathway_slug ); ?>"}} /-->
-
-	<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"},"className":"wporg-query-filters"} -->
-	<div class="wp-block-group wporg-query-filters">
-		<!-- wp:wporg/query-filter {"key":"language"} /-->
-		<!-- wp:wporg/query-filter {"key":"topic"} /-->
-		<!-- wp:wporg/query-filter {"key":"level"} /-->
-	</div>
-	<!-- /wp:group -->
-
-</div>
-<!-- /wp:group -->
-
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60)">
+	<?php } ?>
 
 	<!-- wp:query {"queryId":1,"query":{"perPage":12,"postType":"course","courseFeatured":false,"inherit":true},"namespace":"wporg-learn/course-grid","align":"wide","className":"wporg-learn-course-grid"} -->
 	<div class="wp-block-query alignwide wporg-learn-course-grid">
@@ -122,7 +90,7 @@ if ( isset( $wp_query->query_vars['wporg_learning_pathway'] ) ) {
 		<!-- wp:query-no-results -->
 
 			<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-			<p><?php esc_html_e( 'No Courses found.', 'wporg-learn' ); ?></p>
+			<p><?php $learning_pathway_slug ? esc_html_e( 'No pathways found.', 'wporg-learn' ) : esc_html_e( 'Nothing found.', 'wporg-learn' ); ?></p>
 			<!-- /wp:paragraph -->
 
 		<!-- /wp:query-no-results -->
--- wp-content/themes/pub/wporg-learn-2024/patterns/taxonomy-learning-pathway-content.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/taxonomy-learning-pathway-content.php
@@ -27,46 +27,7 @@ $advanced_level_id = get_term_by( 'slug', 'advanced', 'level' )->term_id;
 $content = get_learning_pathway_level_content( $learning_pathway_slug );
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"0","bottom":"0"}},"border":{"bottom":{"color":"var:preset|color|light-grey-1","width":"1px"}}},"backgroundColor":"<?php echo esc_attr( $learning_pathway_slug ); ?>","className":"wporg-learn-tax-learning-pathway-header","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-<?php echo esc_attr( $learning_pathway_slug ); ?>-background-color has-background wporg-learn-tax-learning-pathway-header" style="border-bottom-color:var(--wp--preset--color--light-grey-1);border-bottom-width:1px;padding-top:0;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:0;padding-left:var(--wp--preset--spacing--edge-space)">
-
-	<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"background":{"backgroundRepeat":"no-repeat","backgroundSize":"contain","backgroundPosition":"100% 50%"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"stretch"}} -->
-	<div class="wp-block-group">
-		
-		<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-
-			<!-- wp:query-title {"type":"archive","showPrefix":false,"fontSize":"heading-1"} /-->
-
-			<!-- wp:term-description {"style":{"typography":{"lineHeight":1.6}}} /-->
-
-		</div>
-		<!-- /wp:group -->
-
-		<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"33%"},"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/learning-pathway-' . $learning_pathway_slug . '.svg' ); ?>","source":"file"},"backgroundPosition":"0% 50%"}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group"></div>
-		<!-- /wp:group -->
-
-	</div>
-	<!-- /wp:group -->
-
-</div>
-<!-- /wp:group -->
-
-<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|50"}}}} -->
-<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--50)">
-
-	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","width":290,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg-learn' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"query":{"wporg_learning_pathway":"<?php echo esc_attr( $learning_pathway_slug ); ?>"}} /-->
-
-	<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"},"className":"wporg-query-filters"} -->
-	<div class="wp-block-group wporg-query-filters">
-		<!-- wp:wporg/query-filter {"key":"topic"} /-->
-		<!-- wp:wporg/query-filter {"key":"level"} /-->
-	</div>
-	<!-- /wp:group -->
-
-</div>
-<!-- /wp:group -->
+<!-- wp:wporg-learn/learning-pathway-header {"align":"full","learningPathwaySlug":"<?php echo esc_attr( $learning_pathway_slug ); ?>"} /-->
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60)">
