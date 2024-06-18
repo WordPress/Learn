554b31893ebf41f857d7ad3346da2348e0480759
ren <18050944+renintw@users.noreply.github.com>
2024-06-18 10:58:53 +0900

    Add contribute page content pattern.

---
+++ wp-content/themes/pub/wporg-learn-2024/patterns/contribute-page-content.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Title: Contribute Page Content
+ * Slug: wporg-learn-2024/contribute-page-content
+ * Inserter: no
+ */
+
+?>
+ 
+<!-- wp:heading {"style":{"typography":{"fontSize":"36px","fontStyle":"normal","fontWeight":"400"}},"fontFamily":"eb-garamond"} -->
+<h2 class="wp-block-heading has-eb-garamond-font-family" style="font-size:36px;font-style:normal;font-weight:400">
+	<?php esc_html_e( 'Contribute', 'wporg-learn' ); ?>
+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"40px","top":"0px"}}},"fontSize":"normal","fontFamily":"inter"} -->
+<p class="has-inter-font-family has-normal-font-size" style="margin-top:0px;margin-bottom:40px;font-style:normal;font-weight:400">
+	<?php esc_html_e( "Here's how you can get involved and create content for Learn WordPress", 'wporg-learn' ); ?>
+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"extra-large","fontFamily":"inter"} -->
+<h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="font-style:normal;font-weight:600">
+	<?php esc_html_e( 'Create a Tutorial', 'wporg-learn' ); ?>
+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"var:preset|spacing|20","top":"var:preset|spacing|20"}}},"fontSize":"small","fontFamily":"inter"} -->
+<p class="has-inter-font-family has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);font-style:normal;font-weight:400">
+	<?php esc_html_e( 'Tutorials can be on any topic related to WordPress, can be for any level of experience, and in any language. To get started, check out this ', 'wporg-learn' ); ?>
+	<a href="https://learn.wordpress.org/workshop/how-to-submit-a-workshop/"><?php esc_html_e( 'tutorial about creating tutorials', 'wporg-learn' ); ?></a><?php esc_html_e( '. Then ', 'wporg-learn' ); ?>
+	<a href="https://learn.wordpress.org/workshop-presenter-application/"><?php esc_html_e( 'submit your tutorial presenter application', 'wporg-learn' ); ?></a><?php esc_html_e( '.', 'wporg-learn' ); ?>
+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:embed {"url":"https://videopress.com/v/52hW5V7s","type":"video","providerNameSlug":"videopress","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-videopress wp-block-embed-videopress wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://videopress.com/v/52hW5V7s
+</div></figure>
+<!-- /wp:embed -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"50px"}}},"fontSize":"extra-large","fontFamily":"inter"} -->
+<h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="margin-top:50px;font-style:normal;font-weight:600">
+	<?php esc_html_e( 'Facilitate an online workshop', 'wporg-learn' ); ?>
+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"0","top":"var:preset|spacing|20"}}},"fontSize":"small","fontFamily":"inter"} -->
+<p class="has-inter-font-family has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:0;font-style:normal;font-weight:400">
+	<?php esc_html_e( 'To learn more about online workshop facilitators, ', 'wporg-learn' ); ?>
+	<a href="https://make.wordpress.org/community/2020/08/11/tuesday-trainings-how-to-be-an-excellent-discussion-group-leader/"><?php esc_html_e( 'read this post', 'wporg-learn' ); ?></a><?php esc_html_e( '. You can ', 'wporg-learn' ); ?>
+	<a href="https://learn.wordpress.org/social-learning/"><?php esc_html_e( 'apply to become an online workshop facilitator here', 'wporg-learn' ); ?></a><?php esc_html_e( ', or you can even ', 'wporg-learn' ); ?>
+	<a href="https://make.wordpress.org/community/handbook/virtual-events/organize-learn-wordpress-discussion-groups-for-your-wordpress-meetup/"><?php esc_html_e( 'organize an online workshop for your local WordPress meetup', 'wporg-learn' ); ?></a><?php esc_html_e( '.', 'wporg-learn' ); ?>
+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"50px"}}},"fontSize":"extra-large","fontFamily":"inter"} -->
+<h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="margin-top:50px;font-style:normal;font-weight:600">
+	<?php esc_html_e( 'Update and contribute lesson plans', 'wporg-learn' ); ?>
+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"var:preset|spacing|20","top":"var:preset|spacing|20"}}},"fontSize":"small","fontFamily":"inter"} -->
+<p class="has-inter-font-family has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);font-style:normal;font-weight:400">
+	<a href="https://learn.wordpress.org/lesson-plans/">Lesson Plans</a><?php esc_html_e( ' provide an outline and complete script for anyone wanting to run their own classes at in-person or virtual events. View our video about submitting Lesson Plans. You can even use these lesson plans for the Tutorials that you submit! The Training team meets weekly in ', 'wporg-learn' ); ?>
+	<a href="http://wordpress.slack.com/messages/training/">#training channel</a><?php esc_html_e( ' in Slack. See their ', 'wporg-learn' ); ?>
+	<a href="https://make.wordpress.org/training/handbook/getting-started/how-we-work-together/"><?php esc_html_e( 'Welcome page for meeting times', 'wporg-learn' ); ?></a><?php esc_html_e( ". Learn more on the Training team's ", 'wporg-learn' ); ?>
+	<a href="https://make.wordpress.org/training/handbook/getting-started/"><?php esc_html_e( 'Get Started page', 'wporg-learn' ); ?></a><?php esc_html_e( '.', 'wporg-learn' ); ?>
+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:embed {"url":"https://videopress.com/v/qkmHo4ug","type":"video","providerNameSlug":"videopress","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-videopress wp-block-embed-videopress wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://videopress.com/v/qkmHo4ug
+</div></figure>
+<!-- /wp:embed -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"50px"}}},"fontSize":"extra-large","fontFamily":"inter"} -->
+<h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="margin-top:50px;font-style:normal;font-weight:600">
+	<?php esc_html_e( 'Update and contribute courses', 'wporg-learn' ); ?>
+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"var:preset|spacing|20","top":"var:preset|spacing|20"}}},"fontSize":"small","fontFamily":"inter"} -->
+<p class="has-inter-font-family has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);font-style:normal;font-weight:400">
+	<a href="https://learn.wordpress.org/courses/"><?php esc_html_e( 'Courses', 'wporg-learn' ); ?></a><?php esc_html_e( ' consist of long-form lessons in multiple media formats - they can include text, video and images. The Training team meets weekly in ', 'wporg-learn' ); ?>
+	<a href="http://wordpress.slack.com/messages/training/"><?php esc_html_e( '#training channel', 'wporg-learn' ); ?></a><?php esc_html_e( ' in Slack. See their ', 'wporg-learn' ); ?>
+	<a href="https://make.wordpress.org/training/handbook/getting-started/how-we-work-together/"><?php esc_html_e( 'Welcome page for meeting times', 'wporg-learn' ); ?></a><?php esc_html_e( ". Learn more on the Training team's ", 'wporg-learn' ); ?>
+	<a href="https://make.wordpress.org/training/handbook/getting-started/"><?php esc_html_e( 'Get Started page', 'wporg-learn' ); ?></a><?php esc_html_e( '.', 'wporg-learn' ); ?>
+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:shortcode -->
+[videopress 9hC1sT88]
+<!-- /wp:shortcode -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"50px"}}},"fontSize":"extra-large","fontFamily":"inter"} -->
+<h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="margin-top:50px;font-style:normal;font-weight:600">
+	<?php esc_html_e( 'Join the Training Team', 'wporg-learn' ); ?>
+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"0","top":"0"}}},"fontSize":"small","fontFamily":"inter"} -->
+<p class="has-inter-font-family has-small-font-size" style="margin-top:0;margin-bottom:0;font-style:normal;font-weight:400">
+	<?php esc_html_e( 'To get even more involved, ', 'wporg-learn' ); ?><strong><?php esc_html_e( 'join the Training Team, ', 'wporg-learn' ); ?></strong><?php esc_html_e( " to help build the platform, review workshop applications, decide on future content, and more. If you're interested in helping, share your interest in the ", 'wporg-learn' ); ?>
+	<a href="http://wordpress.slack.com/messages/training/"><?php esc_html_e( '#training', 'wporg-learn' ); ?></a><?php esc_html_e( ' channel, join one of the Training team meetings, or follow ', 'wporg-learn' ); ?>
+	<a href="https://make.wordpress.org/training/"><?php esc_html_e( 'the Training team', 'wporg-learn' ); ?></a><?php esc_html_e( ' for news on working group-specific meetings.', 'wporg-learn' ); ?>
+</p>
+<!-- /wp:paragraph -->
