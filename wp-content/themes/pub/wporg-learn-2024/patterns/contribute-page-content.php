470f6faba676901b1668205d7806a239d4fea133
ren <18050944+renintw@users.noreply.github.com>
2024-06-20 22:03:44 +0900

    Remove tutorial and lesson plans related section.

--- wp-content/themes/pub/wporg-learn-2024/patterns/contribute-page-content.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/contribute-page-content.php
@@ -19,32 +19,6 @@
 </p>
 <!-- /wp:paragraph -->
 
-<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"extra-large","fontFamily":"inter"} -->
-<h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="font-style:normal;font-weight:600">
-	<?php esc_html_e( 'Create a Tutorial', 'wporg-learn' ); ?>
-</h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"var:preset|spacing|20","top":"var:preset|spacing|20"}}},"fontSize":"small","fontFamily":"inter"} -->
-<p class="has-inter-font-family has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);font-style:normal;font-weight:400">
-	<?php echo wp_kses_post(
-		sprintf(
-			/* translators: 1: tutorial link, 2: application link */
-			__( 'Tutorials can be on any topic related to WordPress, can be for any level of experience, and in any language. 
-			To get started, check out this <a href="%1$s">tutorial about creating tutorials</a>. Then <a href="%2$s">submit your tutorial presenter application</a>.', 'wporg-learn' ),
-			site_url( '/tutorial/how-to-submit-a-workshop/' ),
-			site_url( '/tutorial-presenter-application/' ),
-		)
-	); ?>
-</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:embed {"url":"https://videopress.com/v/52hW5V7s","type":"video","providerNameSlug":"videopress","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-<figure class="wp-block-embed is-type-video is-provider-videopress wp-block-embed-videopress wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
-https://videopress.com/v/52hW5V7s
-</div></figure>
-<!-- /wp:embed -->
-
 <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"50px"}}},"fontSize":"extra-large","fontFamily":"inter"} -->
 <h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="margin-top:50px;font-style:normal;font-weight:600">
 	<?php esc_html_e( 'Facilitate an online workshop', 'wporg-learn' ); ?>
@@ -65,36 +39,6 @@ https://videopress.com/v/52hW5V7s
 	); ?>
 </p>
 <!-- /wp:paragraph -->
-
-<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"50px"}}},"fontSize":"extra-large","fontFamily":"inter"} -->
-<h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="margin-top:50px;font-style:normal;font-weight:600">
-	<?php esc_html_e( 'Update and contribute lesson plans', 'wporg-learn' ); ?>
-</h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"var:preset|spacing|20","top":"var:preset|spacing|20"}}},"fontSize":"small","fontFamily":"inter"} -->
-<p class="has-inter-font-family has-small-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);font-style:normal;font-weight:400">
-	<?php echo wp_kses_post(
-		sprintf(
-			/* translators: 1: lesson plans link, 2: training channel link, 3: welcome page link, 4: get started page link */
-			__( '<a href="%1$s">Lesson Plans</a> provide an outline and complete script for anyone wanting to run their own classes at in-person or virtual events. 
-			View our video about submitting Lesson Plans. You can even use these lesson plans for the Tutorials that you submit! 
-			The Training team meets weekly in <a href="%2$s">#training channel</a> in Slack. See their <a href="%3$s">Welcome page for meeting times</a>. 
-			Learn more on the Training team\'s <a href="%4$s">Get Started page</a>.', 'wporg-learn' ),
-			site_url( '/lesson-plans/' ),
-			'http://wordpress.slack.com/messages/training/',
-			'https://make.wordpress.org/training/handbook/getting-started/how-we-work-together/',
-			'https://make.wordpress.org/training/handbook/getting-started/',
-		)
-	); ?>
-</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:embed {"url":"https://videopress.com/v/qkmHo4ug","type":"video","providerNameSlug":"videopress","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-<figure class="wp-block-embed is-type-video is-provider-videopress wp-block-embed-videopress wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
-https://videopress.com/v/qkmHo4ug
-</div></figure>
-<!-- /wp:embed -->
 
 <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"50px"}}},"fontSize":"extra-large","fontFamily":"inter"} -->
 <h2 class="wp-block-heading has-inter-font-family has-extra-large-font-size" style="margin-top:50px;font-style:normal;font-weight:600">
