7f3f32e347062098232367b60ab08512025db8b1
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-06-26 22:41:21 +1200

    Set the title of the Lesson plan archive

--- wp-content/themes/pub/wporg-learn-2024/patterns/archive-lesson-plan-content.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/archive-lesson-plan-content.php
@@ -10,7 +10,9 @@
 <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left","contentSize":"730px"}} -->
 <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--50)">
 
-	<!-- wp:query-title {"type":"archive","showPrefix":false} /-->
+	<!-- wp:heading {"level":1} -->
+	<h1 class="wp-block-heading"><?php esc_html_e( 'Lesson Plans', 'wporg-learn' ); ?></h1>
+	<!-- /wp:heading -->
 
 	<!-- wp:paragraph -->
 	<p><?php esc_html_e( 'Want to help others learn about WordPress? Read through, use, and remix these lesson plans.', 'wporg-learn' ); ?></p>
