54680bf55b263af4f221edd7337c6237b515e647
ren <18050944+renintw@users.noreply.github.com>
2024-06-30 16:41:23 +0900

    Update progress bar height

--- wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-meta-info.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-meta-info.php
@@ -14,7 +14,7 @@ $course_status = Sensei()->course_progress_repository->get( get_the_ID(), get_cu
 <!-- wp:group {"align":"full","className":"wporg-learn-sidebar-meta-info","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull wporg-learn-sidebar-meta-info">
 
-	<!-- wp:sensei-lms/course-progress {<?php echo ( 'complete' === $course_status ? '"customTextColor":"var(--wp--custom--color--green-50)",' : '' ); ?>"customBarColor":"var(--wp--custom--color--green-50)","height":8,"className":"wporg-learn-sidebar-course-progress"} /-->
+	<!-- wp:sensei-lms/course-progress {<?php echo ( 'complete' === $course_status ? '"customTextColor":"var(--wp--custom--color--green-50)",' : '' ); ?>"customBarColor":"var(--wp--custom--color--green-50)","height":10,"className":"wporg-learn-sidebar-course-progress"} /-->
 
 	<?php if ( Sensei_Course::is_user_enrolled( get_the_ID() ) ) : ?>
 	<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"40px","top":"10px"}},"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":26px}},"textColor":"blueberry-1","fontSize":"normal","fontFamily":"inter","className":""wporg-learn-sidebar-all-courses"} -->
