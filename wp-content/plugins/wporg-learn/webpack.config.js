2b1365374e9b536dfc4cec4661f101c31177b280
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-07-02 10:26:58 +1200

    Add featured lesson post meta field and sidebar control

--- wp-content/plugins/wporg-learn/webpack.config.js
+++ wp-content/plugins/wporg-learn/webpack.config.js
@@ -9,6 +9,7 @@ config.entry = {
 	'duration-meta': './js/duration-meta/index.js',
 	'expiration-date': './js/expiration-date/index.js',
 	'lesson-count': './js/lesson-count/src/index.js',
+	'lesson-featured-meta': './js/lesson-featured-meta/index.js',
 	'workshop-application-form': './js/workshop-application-form/src/index.js',
 	'workshop-details': './js/workshop-details/src/index.js',
 	event: './js/event.js',
