64bb7c9378b9a4c82e59057a4d9c9c7805f515a7
ren <18050944+renintw@users.noreply.github.com>
2024-07-03 00:25:32 +0900

    Fix logged out not displaying icon error

--- wp-content/themes/pub/wporg-learn-2024/src/course-outline/index.php
+++ wp-content/themes/pub/wporg-learn-2024/src/course-outline/index.php
@@ -4,11 +4,17 @@
  * Enqueue scripts and styles.
  */
 function enqueue_assets() {
+	$script_asset_path = get_stylesheet_directory() . '/build/course-outline/index.asset.php';
+	if ( ! file_exists( $script_asset_path ) ) {
+		wp_die( 'You need to run `yarn start` or `yarn build` to build the required assets.' );
+	}
+
+	$script_asset = require( $script_asset_path );
 	wp_enqueue_script(
 		'wporg-learn-2024-course-outline',
 		get_stylesheet_directory_uri() . '/build/course-outline/index.js',
-		array(),
-		filemtime( get_stylesheet_directory() . '/build/course-outline/index.js' ),
+		$script_asset['dependencies'],
+		$script_asset['version'],
 		true
 	);
 
