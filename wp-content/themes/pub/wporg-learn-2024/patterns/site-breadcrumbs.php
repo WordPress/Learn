6d290ef3fd02078ff10c1afefc36394a3f981758
ren <18050944+renintw@users.noreply.github.com>
2024-07-04 03:12:02 +0900

    Ensure breadcrumbs are displayed only when there are at least 3 levels.

---
+++ wp-content/themes/pub/wporg-learn-2024/patterns/site-breadcrumbs.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Site Breadcrumbs
+ * Slug: wporg-learn-2024/site-breadcrumbs
+ * Inserter: no
+ */
+
+// Ensure breadcrumbs are displayed only when there are at least 3 levels.
+$block_content = '<!-- wp:wporg/site-breadcrumbs {"fontSize":"small"} /-->';
+$parsed_content = do_blocks( $block_content );
+preg_match_all( '/<span\b[^>]*>/', $parsed_content, $matches );
+$breadcrumb_level = count( $matches[0] );
+$show_breadcrumbs = $breadcrumb_level >= 3;
+
+?>
+
+<?php if ( $show_breadcrumbs ) : ?>
+	<!-- wp:group {"className":"wporg-breadcrumbs","align":"full","style":{"spacing":{"padding":{"top":"23px","bottom":"23px","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}},"backgroundColor":"white","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+	<div class="wporg-breadcrumbs wp-block-group alignfull has-white-background-color has-background" style="padding-top:23px;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:23px;padding-left:var(--wp--preset--spacing--edge-space)">
+		<!-- wp:wporg/site-breadcrumbs {"fontSize":"small"} /-->
+	</div>
+	<!-- /wp:group -->
+<?php endif; ?>
