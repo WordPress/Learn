a4426a2687555494330d52a17c8011040d458254
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-07-12 15:34:35 +1200

    Prioritize Courses, Lessons, Lesson Plans and Tutorials matching user's locale (#2657)
    * Move the tutorial locale prioritization to the plugin
    
    Enables this functionality on the new theme as well.
    
    * Refactor to allow adding more post types to filter
    
    * Prioritize lessons archive by locale
    
    * Change lessons priority for english locale
    
    Change the query to prioritize lessons with empty values over other languages if the locale is english.
    The default language post meta value is english, so if empty this is implied.
    
    * Prioritize Courses by locale
    
    * Fix typo
    
    Co-authored-by: Ren <18050944+renintw@users.noreply.github.com>
    
    * :lipstick: Update docs
    
    * Prioritize lesson plans by locale
    
    * Block new prioritization with old theme
    
    ---------
    
    Co-authored-by: Ren <18050944+renintw@users.noreply.github.com>

--- wp-content/plugins/wporg-learn/inc/locale.php
+++ wp-content/plugins/wporg-learn/inc/locale.php
@@ -14,6 +14,7 @@ add_filter( 'wporg_learn_update_locale_data', __NAMESPACE__ . '\update_locale_da
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_assets' );
 add_filter( 'wporg_locale_switcher_options', __NAMESPACE__ . '\locale_switcher_options' );
 add_filter( 'wp_headers', __NAMESPACE__ . '\disable_caching' );
+add_filter( 'posts_clauses', __NAMESPACE__ . '\wporg_archive_query_prioritize_locale', 10, 2 );
 
 if ( ! wp_next_scheduled( 'wporg_learn_update_locale_data' ) ) {
 	wp_schedule_event( time(), 'hourly', 'wporg_learn_update_locale_data' );
@@ -155,4 +156,180 @@ function disable_caching( $headers ) {
 	}
 
 	return $headers;
+}
+
+/**
+ * Modify post type archive queries to prioritize content in the user's locale.
+ *
+ * @param array    $clauses
+ * @param WP_Query $query
+ *
+ * @return array
+ */
+function wporg_archive_query_prioritize_locale( $clauses, $query ) {
+	if ( is_admin() || is_feed() ) {
+		return $clauses;
+	}
+
+	$locale = get_locale();
+
+	if ( ! $locale ) {
+		return $clauses;
+	}
+
+	if ( $query->is_post_type_archive( 'wporg_workshop' ) ) {
+		return wporg_tutorials_query_prioritize_locale( $clauses, $locale );
+	}
+
+	$current_theme = wp_get_theme();
+	$theme_slug = $current_theme->get_stylesheet();
+
+	if ( 'pub/wporg-learn-2020' === $theme_slug ) {
+		return $clauses;
+	}
+
+	if ( $query->is_post_type_archive( 'course' ) || $query->is_post_type_archive( 'lesson' ) || $query->is_post_type_archive( 'lesson-plan' ) ) {
+		return wporg_query_prioritize_locale( $clauses, $locale );
+	}
+
+	return $clauses;
+}
+
+/**
+ * Modify the workshop post type archive query to prioritize workshops in the user's locale.
+ * DEPRECATED: this function has been superseded by wporg_query_prioritize_locale, which handles language post meta being empty or null.
+ *
+ * In order to show all workshops, but with the ones that are presented/captioned in the user's locale shown first, we
+ * need to modify the posts query in ways that can't be done through the WP_Query or WP_Meta_Query APIs. Instead, here,
+ * we're filtering the individual clauses of the query to add the pieces we need.
+ *
+ * Examples, slightly truncated for simplicity:
+ *
+ * Before:
+ * SELECT SQL_CALC_FOUND_ROWS wp_posts.ID
+ * FROM wp_posts
+ * WHERE 1=1
+ * AND wp_posts.post_type = 'wporg_workshop'
+ * ORDER BY wp_posts.post_date DESC
+ *
+ * After:
+ * SELECT SQL_CALC_FOUND_ROWS wp_posts.*,
+ *   MAX( IF( pmeta.meta_key = 'language' AND pmeta.meta_value LIKE 'art_%', 1, 0 ) ) AS has_language,
+ *   MAX( IF( pmeta.meta_key = 'video_caption_language' AND pmeta.meta_value LIKE 'art_%', 1, 0 ) ) AS has_caption
+ * FROM wp_posts
+ * INNER JOIN wp_postmeta pmeta ON ( wp_posts.ID = pmeta.post_id )
+ * WHERE 1=1
+ * AND wp_posts.post_type = 'wporg_workshop'
+ * GROUP BY wp_posts.ID
+ * ORDER BY has_language DESC, has_caption DESC, wp_posts.post_date DESC
+ *
+ * @param array  $clauses
+ * @param string $locale
+ *
+ * @return array
+ */
+function wporg_tutorials_query_prioritize_locale( $clauses, $locale ) {
+	global $wpdb;
+
+	$locale_root = preg_replace( '#^([a-z]{2,3}_?)[a-zA-Z_-]*#', '$1', $locale, -1, $count );
+
+	if ( $count ) {
+		/**
+		 * $clauses['fields'] contains the SELECT part of the query.
+		 *
+		 * The extra fields clauses are calculated fields that will contain a `1` if the workshop post row has a postmeta
+		 * value that matches the locale root. The MAX() and the groupby clause below ensure that all the rows for a
+		 * given workshop are consolidated into one with the highest value in the calculated column. Without the
+		 * grouping, there would be a separate row for each postmeta value for each workshop post.
+		 */
+		$clauses['fields'] .= ",
+			MAX( IF( pmeta.meta_key = 'language' AND pmeta.meta_value LIKE '{$locale_root}%', 1, 0 ) ) AS has_language
+		";
+		$clauses['fields'] .= ",
+			MAX( IF( pmeta.meta_key = 'video_caption_language' AND pmeta.meta_value LIKE '{$locale_root}%', 1, 0 ) ) AS has_caption
+		";
+		$clauses['join']   .= " INNER JOIN {$wpdb->postmeta} pmeta ON ( {$wpdb->posts}.ID = pmeta.post_id )";
+		// This orderby clause ensures that the workshops are sorted by the values in the calculated columns first.
+		$clauses['orderby'] = 'has_language DESC, has_caption DESC, ' . $clauses['orderby'];
+
+		if ( false === strpos( $clauses['groupby'], "{$wpdb->posts}.ID" ) ) {
+			$clauses['groupby'] = "{$wpdb->posts}.ID";
+		}
+	}
+
+	return $clauses;
+}
+
+/**
+ * Modify the post type archive query to prioritize posts in the user's locale.
+ *
+ * In order to show all posts, but with the ones that are presented in the user's locale shown first, we
+ * need to modify the posts query in ways that can't be done through the WP_Query or WP_Meta_Query APIs. Instead, here,
+ * we're filtering the individual clauses of the query to add the pieces we need.
+ *
+ * Handles cases where the language post meta is empty or null and treats them as being english, if the locale is english.
+ * This matches the default value for the language post meta field.
+ *
+ * Examples, slightly truncated for simplicity:
+ *
+ * Before:
+ * SELECT SQL_CALC_FOUND_ROWS wp_posts.ID
+ * FROM wp_posts
+ * WHERE 1=1
+ * AND wp_posts.post_type = 'lesson'
+ * ORDER BY wp_posts.post_date DESC
+ *
+ * After:
+ * SELECT SQL_CALC_FOUND_ROWS wp_posts.*,
+ *   MAX(
+ *     CASE
+ *       WHEN pmeta.meta_key = 'language' AND pmeta.meta_value LIKE 'en_%' THEN 1
+ *       WHEN (pmeta.meta_key = 'language' AND pmeta.meta_value = '') OR pmeta.meta_key IS NULL THEN 1
+ *       ELSE 0
+ *     END
+ *   ) AS has_language,
+ * FROM wp_posts
+ * LEFT JOIN wp_postmeta pmeta ON ( wp_posts.ID = pmeta.post_id AND pmeta.meta_key = 'language' )
+ * WHERE 1=1
+ * AND wp_posts.post_type = 'lesson'
+ * ORDER BY has_language DESC, wp_posts.post_date DESC
+ * GROUP BY wp_posts.ID
+ *
+ * @param array  $clauses
+ * @param string $locale
+ *
+ * @return array
+ */
+function wporg_query_prioritize_locale( $clauses, $locale ) {
+	global $wpdb;
+
+	$locale_root = preg_replace( '#^([a-z]{2,3}_?)[a-zA-Z_-]*#', '$1', $locale, -1, $count );
+
+	if ( $count ) {
+		/**
+		 * $clauses['fields'] contains the SELECT part of the query.
+		 *
+		 * The extra fields clause is calculated, and will contain a `1` if the post row has a postmeta
+		 * value that matches the locale root, or if the locale is english and the postmeta value is empty or null.
+		 * The MAX() and the groupby clause below ensure that all the rows for a given post are consolidated into
+		 * one, with the highest value in the calculated column.
+		 * Without the grouping, there would be a separate row for each postmeta value for each post.
+		 */
+		$is_english = strpos( $locale_root, 'en_' ) === 0;
+
+		$clauses['fields'] .= ",
+			MAX(
+				CASE
+					WHEN pmeta.meta_key = 'language' AND pmeta.meta_value LIKE '{$locale_root}%' THEN 1
+					WHEN (pmeta.meta_key = 'language' AND pmeta.meta_value = '') OR pmeta.meta_key IS NULL THEN " . ( $is_english ? '1' : '0' ) . '
+					ELSE 0
+				END
+			) AS has_language
+		';
+		$clauses['join']   .= " LEFT JOIN {$wpdb->postmeta} pmeta ON ( {$wpdb->posts}.ID = pmeta.post_id AND pmeta.meta_key = 'language' )";
+		$clauses['orderby'] = 'has_language DESC, ' . $clauses['orderby'];
+		$clauses['groupby'] = "{$wpdb->posts}.ID";
+	}
+
+	return $clauses;
 }
