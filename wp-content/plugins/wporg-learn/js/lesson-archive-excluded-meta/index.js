74ac4ff627bc49c1620dad44acd8b7038c707daa
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-07-15 14:46:31 +1200

    Add a post meta filter to exclude a lesson from archive views (#2658)

---
+++ wp-content/plugins/wporg-learn/js/lesson-archive-excluded-meta/index.js
@@ -0,0 +1,42 @@
+/**
+ * WordPress dependencies
+ */
+import { CheckboxControl, PanelRow } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { registerPlugin } from '@wordpress/plugins';
+
+const EXCLUDED = 'excluded';
+
+const LessonArchiveExcludedMeta = () => {
+	const postMetaData = useSelect( ( select ) => select( 'core/editor' ).getEditedPostAttribute( 'meta' ) || {} );
+	const { editPost } = useDispatch( 'core/editor' );
+	const [ lessonExcluded, setLessonFeatured ] = useState( postMetaData?._lesson_archive_excluded === EXCLUDED );
+
+	return (
+		<PluginDocumentSettingPanel title={ __( 'Hidden Lesson', 'wporg-learn' ) }>
+			<PanelRow>
+				<CheckboxControl
+					label={ __( 'Exclude this lesson from the archive', 'wporg-learn' ) }
+					checked={ lessonExcluded }
+					onChange={ ( newLessonExcluded ) => {
+						setLessonFeatured( newLessonExcluded );
+
+						editPost( {
+							meta: {
+								...postMetaData,
+								_lesson_archive_excluded: newLessonExcluded ? EXCLUDED : '',
+							},
+						} );
+					} }
+				/>
+			</PanelRow>
+		</PluginDocumentSettingPanel>
+	);
+};
+
+registerPlugin( 'wporg-learn-lesson-archive-excluded-meta', {
+	render: LessonArchiveExcludedMeta,
+} );
--- wp-content/themes/pub/wporg-learn-2024/inc/query.php
+++ wp-content/themes/pub/wporg-learn-2024/inc/query.php
@@ -5,15 +5,16 @@
 
 namespace WordPressdotorg\Theme\Learn_2024\Query;
 
-add_action( 'pre_get_posts', __NAMESPACE__ . '\modify_archive_queries' );
-add_action( 'pre_get_posts', __NAMESPACE__ . '\modify_level_query' );
+add_action( 'pre_get_posts', __NAMESPACE__ . '\add_language_to_archive_queries' );
+add_action( 'pre_get_posts', __NAMESPACE__ . '\handle_all_level_query' );
+add_action( 'pre_get_posts', __NAMESPACE__ . '\add_excluded_to_lesson_archive_query' );
 
 /**
  * Modify the query by adding meta query for language if set.
  *
  * @param WP_Query $query The query object.
  */
-function modify_archive_queries( $query ) {
+function add_language_to_archive_queries( $query ) {
 	// Ensure this code runs only for the main query on archive pages and search results.
 	if ( ! is_admin() && $query->is_main_query() && ( $query->is_archive() || $query->is_search() ) ) {
 		if ( isset( $_GET['language'] ) && is_array( $_GET['language'] ) ) {
@@ -39,6 +40,8 @@ function modify_archive_queries( $query ) {
 			$query->set( 'meta_query', $meta_query );
 		}
 	}
+
+	return $query;
 }
 
 /**
@@ -48,7 +51,7 @@ function modify_archive_queries( $query ) {
  * @param WP_Query $query The main query.
  * @return WP_Query
  */
-function modify_level_query( $query ) {
+function handle_all_level_query( $query ) {
 	if ( is_admin() || ! $query->is_main_query() ) {
 		return;
 	}
@@ -57,6 +60,34 @@ function modify_level_query( $query ) {
 
 	if ( 'all' === $level ) {
 		$query->set( 'wporg_lesson_level', '' );
+	}
+
+	return $query;
+}
+
+/**
+ * Modify the query by adding meta query for excluding the lesson from the archive if set.
+ *
+ * @param WP_Query $query The query object.
+ */
+function add_excluded_to_lesson_archive_query( $query ) {
+	// Ensure this code runs only for the main query on lesson archive pages and search results.
+	if ( ! is_admin() && $query->is_main_query() && ( $query->is_archive( 'lesson' ) || $query->is_search() ) ) {
+		$query->set(
+			'meta_query',
+			array(
+				'relation' => 'OR',
+				array(
+					'key'     => '_lesson_archive_excluded',
+					'compare' => 'NOT EXISTS',
+				),
+				array(
+					'key'     => '_lesson_archive_excluded',
+					'value'   => 'excluded',
+					'compare' => '!=',
+				),
+			)
+		);
 	}
 
 	return $query;
