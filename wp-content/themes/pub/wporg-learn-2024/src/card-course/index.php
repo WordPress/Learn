d33d302bfe586829cd82263db53da5ee58da5386
ren <18050944+renintw@users.noreply.github.com>
2024-07-26 06:51:48 +0900

    Apply to my courses page content pattern

--- wp-content/themes/pub/wporg-learn-2024/src/card-course/index.php
+++ wp-content/themes/pub/wporg-learn-2024/src/card-course/index.php
@@ -25,12 +25,26 @@ function init() {
 
 	$script_handle = register_block_script_handle( $metadata, 'viewScript', 0 );
 
-	// Enqueue the assets only when the card course template is on the page.
+	// Enqueue the assets when the card course template is on the page.
 	add_action(
 		'render_block_core/template-part',
 		function( $block_content, $block ) use ( $script_handle ) {
 			$slugs = array( 'card-course-h3', 'card-course', 'card-lesson-h3', 'card-lesson', 'card' );
 			if ( isset( $block['attrs']['slug'] ) && in_array( $block['attrs']['slug'], $slugs, true ) ) {
+				wp_enqueue_script( $script_handle );
+			}
+			return $block_content;
+		},
+		10,
+		2
+	);
+
+	// Enqueue the assets when the my courses page content pattern is on the page.
+	// The blocks composition of this pattern is similar to that of the card course.
+	add_action(
+		'render_block_core/pattern',
+		function( $block_content, $block ) use ( $script_handle ) {
+			if ( isset( $block['attrs']['slug'] ) && 'wporg-learn-2024/page-my-courses-content' === $block['attrs']['slug'] ) {
 				wp_enqueue_script( $script_handle );
 			}
 			return $block_content;
