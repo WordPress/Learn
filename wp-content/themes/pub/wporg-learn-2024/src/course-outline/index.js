50d69fc5fd5075169dfa3a493989d8b686a20b59
Ren <18050944+renintw@users.noreply.github.com>
2024-08-01 00:15:33 +0800

    Course outline block enhance (#2776)
    * Make headers collapse/open when clicked anywhere.
    
    * Fix a11y.
    
    * Fix focus ring on lesson item gets hidden.
    
    * Fix the button focus ring size too small on Firefox.
    
    * Remove module heading link.
    
    * Remove the link within the button span with class "screen-reader-text" if it exists

--- wp-content/themes/pub/wporg-learn-2024/src/course-outline/index.js
+++ wp-content/themes/pub/wporg-learn-2024/src/course-outline/index.js
@@ -5,6 +5,40 @@ import { renderToString } from '@wordpress/element';
 
 document.addEventListener( 'DOMContentLoaded', () => {
 	/**
+	 * Allow the entire header to toggle, giving users a larger area to interact with.
+	 */
+	document
+		.querySelectorAll( 'section.wp-block-sensei-lms-course-outline-module-bordered > header' )
+		.forEach( ( header ) => {
+			const button = header.querySelector( 'button' );
+			header.addEventListener( 'click', () => {
+				button.click();
+			} );
+
+			button.addEventListener( 'click', ( event ) => {
+				event.stopPropagation();
+			} );
+
+			// To enable the entire header to be clickable for toggling without conflicts, remove the link.
+			// In fact, this link duplicates the first lesson link in the course outline.
+			// See https://github.com/WordPress/Learn/pull/2776#issuecomment-2258308422
+			const link = header.querySelector( 'h2 > a' );
+			if ( link ) {
+				const heading = link.parentElement;
+				heading.innerHTML = link.innerHTML;
+			}
+			// Also remove the link within the span with class "screen-reader-text" if it exists.
+			const span = header.querySelector( 'span.screen-reader-text' );
+			if ( span ) {
+				const spanLink = span.querySelector( 'a' );
+				if ( spanLink ) {
+					const spanContent = spanLink.innerHTML;
+					span.innerHTML = spanContent;
+				}
+			}
+		} );
+
+	/**
 	 * Find all in progress lessons, and replace the status icon with the Gutenberg-style `drafts` icon.
 	 */
 	document.querySelectorAll( '.wp-block-sensei-lms-course-outline-lesson.is-in-progress' ).forEach( ( link ) => {
