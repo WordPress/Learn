6ed3a3dad704489995c65b31587c010f89e13658
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-08-08 15:46:10 +1200

    Lesson actions: Swap order of primary and secondary
    Move 'Take quiz' before 'Complete lesson' as it is the primary action.
    
    Fixes https://github.com/WordPress/Learn/issues/2739

--- wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-actions.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-actions.php
@@ -17,13 +17,6 @@ $is_completed = Sensei_Utils::user_completed_lesson( get_the_ID() );
 <!-- wp:sensei-lms/lesson-actions {"toggledBlocks":{"sensei-lms/button-reset-lesson":false}} -->
 <div class="wp-block-sensei-lms-lesson-actions">
 	<div class="sensei-buttons-container">
-		<!-- wp:sensei-lms/button-complete-lesson {"inContainer":true,"className":"is-style-outline"} -->
-		<div class="wp-block-sensei-lms-button-complete-lesson is-style-outline sensei-buttons-container__button-block wp-block-sensei-lms-button-complete-lesson__wrapper">
-			<div class="wp-block-sensei-lms-button-complete-lesson is-style-outline wp-block-sensei-button wp-block-button has-text-align-left">
-				<button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Complete lesson', 'sensei-lms' ); ?></button>
-			</div>
-		</div>
-		<!-- /wp:sensei-lms/button-complete-lesson -->
 
 		<!-- wp:sensei-lms/button-view-quiz {"inContainer":true} -->
 		<div class="wp-block-sensei-lms-button-view-quiz is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-view-quiz__wrapper">
@@ -32,6 +25,14 @@ $is_completed = Sensei_Utils::user_completed_lesson( get_the_ID() );
 			</div>
 		</div>
 		<!-- /wp:sensei-lms/button-view-quiz -->
+
+		<!-- wp:sensei-lms/button-complete-lesson {"inContainer":true,"className":"is-style-outline"} -->
+		<div class="wp-block-sensei-lms-button-complete-lesson is-style-outline sensei-buttons-container__button-block wp-block-sensei-lms-button-complete-lesson__wrapper">
+			<div class="wp-block-sensei-lms-button-complete-lesson is-style-outline wp-block-sensei-button wp-block-button has-text-align-left">
+				<button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Complete lesson', 'sensei-lms' ); ?></button>
+			</div>
+		</div>
+		<!-- /wp:sensei-lms/button-complete-lesson -->
 
 		<?php if ( $is_completed && ( $prev_url || $next_url ) ) : ?>
 			<!-- wp:buttons {"className":"sensei-lesson-actions-nav"} -->
