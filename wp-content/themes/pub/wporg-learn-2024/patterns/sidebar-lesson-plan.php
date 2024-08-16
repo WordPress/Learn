a3b2835c96e23c91c63932a6d78bd4f94fadf46f
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-08-16 14:43:00 +1200

    Theme: Update playground demo links
    Fixes an issue with the current demo URLs loading and expecting a PR number.

--- wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-lesson-plan.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-lesson-plan.php
@@ -77,7 +77,7 @@ $current_post = get_post();
 		<div class="wp-block-buttons">
 			<!-- wp:button {"textAlign":"center","width":100,"style":{"border":{"radius":"2px"},"spacing":{"padding":{"left":"13px","right":"13px","top":"16px","bottom":"16px"}},"typography":{"lineHeight":0,"fontStyle":"normal","fontWeight":"400"}},"className":"aligncenter is-style-outline","fontSize":"normal","fontFamily":"inter"} -->
 			<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size aligncenter is-style-outline has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400;line-height:0">
-				<a class="wp-block-button__link has-text-align-center wp-element-button" href="https://wordpress.org/playground/demo/?step=playground&amp;theme=twentytwentythree" style="border-radius:2px;padding-top:16px;padding-right:13px;padding-bottom:16px;padding-left:13px" target="_blank" rel="noreferrer noopener">
+				<a class="wp-block-button__link has-text-align-center wp-element-button" href="https://playground.wordpress.net/?networking=yes" style="border-radius:2px;padding-top:16px;padding-right:13px;padding-bottom:16px;padding-left:13px" target="_blank" rel="noreferrer noopener">
 					<?php esc_html_e( 'Practice on a private demo site', 'wporg-learn' ); ?>
 				</a>
 			</div>
--- wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-tutorial.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-tutorial.php
@@ -17,7 +17,7 @@
 		
 		<!-- wp:button {"textAlign":"center","width":100,"style":{"typography":{"lineHeight":0,"fontStyle":"normal","fontWeight":"400"},"spacing":{"padding":{"left":"var:preset|spacing|10","right":"var:preset|spacing|10"}}},"className":"aligncenter is-style-outline","fontFamily":"inter"} -->
 		<div class="wp-block-button has-custom-width wp-block-button__width-100 aligncenter is-style-outline has-inter-font-family" style="font-style:normal;font-weight:400">
-			<a class="wp-block-button__link has-text-align-center wp-element-button" href="https://wordpress.org/playground/demo/?step=playground&amp;theme=twentytwentythree" style="padding-right:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)" target="_blank" rel="noreferrer noopener"><?php esc_html_e( 'Practice on a private demo site', 'wporg-learn' ); ?></a>
+			<a class="wp-block-button__link has-text-align-center wp-element-button" href="https://playground.wordpress.net/?networking=yes" style="padding-right:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)" target="_blank" rel="noreferrer noopener"><?php esc_html_e( 'Practice on a private demo site', 'wporg-learn' ); ?></a>
 		</div>
 		<!-- /wp:button -->
 	
