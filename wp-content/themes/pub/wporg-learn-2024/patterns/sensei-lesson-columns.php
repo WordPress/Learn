6e6ac77f3c7db0d5924bc2e2428d8152e2225d34
ren <18050944+renintw@users.noreply.github.com>
2024-09-03 03:32:28 +0900

    Fix all layout issues for single lesson while login & non-login

--- wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-columns.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-columns.php
@@ -5,12 +5,15 @@
  * Inserter: no
  */
 
+$lesson_id = Sensei_Utils::get_current_lesson();
+$module = Sensei()->modules->get_lesson_module( $lesson_id );
+
 ?>
 
 <!-- wp:sensei-lms/ui {"elementClass":"sensei-course-theme__columns","className":"sensei-version\u002d\u002d4-16-2"} -->
 <div class="wp-block-sensei-lms-ui sensei-course-theme__columns sensei-version--4-16-2">
 
-	<!-- wp:sensei-lms/ui {"elementClass":"sensei-course-theme__sidebar","className":"","style"={"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|30","bottom":"var:preset|spacing|50","left":"var:preset|spacing|edge-space"}}}} -->
+	<!-- wp:sensei-lms/ui {"elementClass":"sensei-course-theme__sidebar","style"={"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|30","bottom":"var:preset|spacing|50","left":"var:preset|spacing|edge-space"}}}} -->
 	<div class="wp-block-sensei-lms-ui sensei-course-theme__frame sensei-course-theme__sidebar" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--edge-space)">
 
 		<!-- wp:sensei-lms/course-navigation /-->
@@ -23,19 +26,23 @@
 
 		<!-- wp:sensei-lms/course-theme-lesson-module /-->
 
-		<!-- wp:post-title {"level":1,"fontSize":"heading-1"} /-->
+		<?php if ( $module ) : ?>
+			<!-- wp:post-title {"level":1,"fontSize":"heading-1"} /-->
+		<?php else : ?>
+			<!-- wp:post-title {"level":1,"fontSize":"heading-1","style":{"spacing":{"margin":{"top":"0"}},"typography":{"lineHeight":"1"}}} /-->	 
+		<?php endif; ?> 
 
 		<!-- wp:sensei-lms/course-theme-notices /-->
 
 		<!-- wp:post-content {"layout":{"inherit":true}} /-->
 
 		<?php if ( is_user_logged_in() ) : ?>
-			<!-- wp:group {"style":{"spacing":{"margin":{"top":"40px"}}},"layout":{"type":"constrained"},"className":"sensei-lesson-footer"} -->
-			<div class="wp-block-group sensei-lesson-footer" style="margin-top:40px">
+			<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"var(--wp--preset--spacing--50)"}}},"layout":{"type":"constrained"},"className":"sensei-lesson-footer"} -->
+			<div class="wp-block-group sensei-lesson-footer" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--50)">
 				<!-- wp:sensei-lms/page-actions {"style":{"spacing":{"blockGap":"43px"}}} /-->
 
-				<!-- wp:group {"style":{"spacing":{"margin":{"top":"20px"}}}} -->
-				<div class="wp-block-group" style="margin-top:20px">
+				<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+				<div class="wp-block-group" style="margin-top:0">
 
 					<!-- wp:pattern {"slug":"wporg-learn-2024/sensei-lesson-actions"} /-->
 
@@ -45,11 +52,11 @@
 			<!-- /wp:group -->
 		<?php endif; ?>
 
-		<!-- wp:group {"align":"full","style":{"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"},"right":{},"bottom":{},"left":{}},"spacing":{"margin":{"top":"var:preset|spacing|20"},"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--20);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--edge-space);border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px">
+		<!-- wp:group {"align":"full","style":{"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"},"right":{},"bottom":{},"left":{}},"spacing":{"margin":{"top":"0"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group alignfull" style="margin-top:0;border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px;order:2">
 
-			<!-- wp:group {"layout":{"type":"constrained"}} -->
-			<div class="wp-block-group alignwide">
+			<!-- wp:group {"layout":{"type":"constrained"},"spacing":{"margin":{"top":"var(--wp--preset--spacing--30)"}}} -->
+			<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--30)">
 
 				<!-- wp:pattern {"slug":"wporg-learn-2024/content-feedback"} /-->
 
