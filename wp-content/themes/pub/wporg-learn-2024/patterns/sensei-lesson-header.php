d490cd8b7d6365f8570051f16ff7d3c92bf40f6f
ren <18050944+renintw@users.noreply.github.com>
2024-10-14 18:07:15 +0900

    Adjust p font size

--- wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-header.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-header.php
@@ -38,8 +38,8 @@ $is_user_enrolled = Sensei_Course::is_user_enrolled( $course_id );
 					<h2 class="wp-block-heading has-large-font-size" style="line-height:1.3"><?php esc_html_e( 'Facilitator notes', 'wporg-learn' ); ?></h2>
 					<!-- /wp:heading -->
 
-					<!-- wp:paragraph -->
-					<p>Fake content. Sint aliquip commodo excepteur Lorem. Nostrud proident sint excepteur labore ea eu reprehenderit voluptate amet. Lorem fugiat quis ex culpa incididunt officia duis reprehenderit consectetur. In eiusmod sunt pariatur veniam dolor ut adipisicing sunt. Enim esse reprehenderit pariatur consequat duis excepteur exercitation.<br>Nulla consectetur esse reprehenderit tempor.<br>Nisi mollit ipsum nisi non deserunt proident eu sit amet exercitation laboris.<br>Commodo laborum fugiat in. Sit cupidatat esse do aliquip est ullamco qui ullamco adipisicing ad sunt anim tempor.<br>Tempor sint in mollit non in qui tempor esse culpa ipsum consectetur excepteur minim. Ipsum et ut exercitation enim et commodo id exercitation. Ad magna ad eiusmod ipsum culpa est dolor exercitation nostrud consequat elit aliquip.</p>
+					<!-- wp:paragraph {"fontSize":"normal"} -->
+					<p class="has-normal-font-size">Fake content. Sint aliquip commodo excepteur Lorem. Nostrud proident sint excepteur labore ea eu reprehenderit voluptate amet. Lorem fugiat quis ex culpa incididunt officia duis reprehenderit consectetur. In eiusmod sunt pariatur veniam dolor ut adipisicing sunt. Enim esse reprehenderit pariatur consequat duis excepteur exercitation.<br>Nulla consectetur esse reprehenderit tempor.<br>Nisi mollit ipsum nisi non deserunt proident eu sit amet exercitation laboris.<br>Commodo laborum fugiat in. Sit cupidatat esse do aliquip est ullamco qui ullamco adipisicing ad sunt anim tempor.<br>Tempor sint in mollit non in qui tempor esse culpa ipsum consectetur excepteur minim. Ipsum et ut exercitation enim et commodo id exercitation. Ad magna ad eiusmod ipsum culpa est dolor exercitation nostrud consequat elit aliquip.</p>
 					<!-- /wp:paragraph -->
 				</div>
 				<!-- /wp:group -->
