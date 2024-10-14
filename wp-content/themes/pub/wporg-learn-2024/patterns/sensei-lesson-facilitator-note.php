b94eb16b05e8ef516ab4103caf08da1cc5fab7d8
ren <18050944+renintw@users.noreply.github.com>
2024-10-15 05:54:38 +0900

    Add backdrop & close button

--- wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-facilitator-note.php
+++ wp-content/themes/pub/wporg-learn-2024/patterns/sensei-lesson-facilitator-note.php
@@ -8,11 +8,13 @@
 ?>
 
 <input type="checkbox" id="wporg-learn-facilitator-notes-toggle">
-<label for="wporg-learn-facilitator-notes-toggle" id="wporg-learn-facilitator-notes-label"><?php esc_html_e( 'Facilitator Notes', 'wporg-learn' ); ?></label>
+<label for="wporg-learn-facilitator-notes-toggle" class="wporg-learn-facilitator-notes-label"><?php esc_html_e( 'Facilitator Notes', 'wporg-learn' ); ?></label>
    
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|20"}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
-<div id="wporg-learn-facilitator-notes-content" class="wp-block-group has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40)">
-	<!-- wp:heading {"style":{"typography":{"lineHeight":"1.3"}},"fontSize":"large"} -->
+<div class="wporg-learn-facilitator-notes-content wp-block-group has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40)">
+	<!-- wp:heading {"className":"wporg-learn-facilitator-notes-content","style":{"typography":{"lineHeight":"1.3"}},"fontSize":"large"} -->
+	<div class="wporg-learn-facilitator-notes-close"></div>
+
 	<h2 class="wp-block-heading has-large-font-size" style="line-height:1.3"><?php esc_html_e( 'Facilitator notes', 'wporg-learn' ); ?></h2>
 	<!-- /wp:heading -->
 
@@ -23,3 +25,5 @@
 	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
+
+<div class="wporg-learn-facilitator-notes-backdrop"></div>
