482c66611290043e9af98332a1c96286514d2f33
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-11-28 16:15:18 +1300

    Wire up meta boxes

--- wp-content/plugins/wporg-learn/inc/post-meta.php
+++ wp-content/plugins/wporg-learn/inc/post-meta.php
@@ -19,6 +19,7 @@ add_action( 'add_meta_boxes', __NAMESPACE__ . '\add_lesson_plan_metaboxes' );
 add_action( 'add_meta_boxes', __NAMESPACE__ . '\add_workshop_metaboxes' );
 add_action( 'add_meta_boxes', __NAMESPACE__ . '\add_meeting_metaboxes' );
 add_action( 'save_post_lesson-plan', __NAMESPACE__ . '\save_lesson_plan_metabox_fields' );
+add_action( 'save_post_lesson', __NAMESPACE__ . '\save_lesson_meta_fields' );
 add_action( 'save_post_wporg_workshop', __NAMESPACE__ . '\save_workshop_meta_fields' );
 add_action( 'save_post_meeting', __NAMESPACE__ . '\save_meeting_metabox_fields' );
 add_action( 'admin_footer', __NAMESPACE__ . '\render_locales_list' );
@@ -695,7 +696,52 @@ function save_workshop_meta_fields( $post_id ) {
 
 	// This language meta field is rendered in the editor sidebar using a PluginDocumentSettingPanel block,
 	// which won't save the field on publish if it has the default value.
-	// Our custom workshops query for locale prioritized tutorials (see functions.php `wporg_archive_query_prioritize_locale`)
+	// Our custom query for locale prioritized tutorials (see locale.php `wporg_archive_query_prioritize_locale`)
+	// depends on it being set, so we force it to be updated after saving:
+	$language         = get_post_meta( $post_id, 'language', true );
+	$language_default = 'en_US';
+	if ( ! isset( $language ) || $language_default === $language ) {
+		update_post_meta( $post_id, 'language', $language_default );
+	}
+}
+
+/**
+ * Update the post meta values from the meta fields when the post is saved.
+ *
+ * @param int $post_id
+ */
+function save_lesson_meta_fields( $post_id ) {
+	if ( wp_is_post_revision( $post_id ) || ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
+
+	// This nonce field is rendered in the Lesson Video metabox.
+	$nonce = filter_input( INPUT_POST, 'lesson-metabox-nonce' );
+	if ( ! wp_verify_nonce( $nonce, 'lesson-metaboxes' ) ) {
+		return;
+	}
+
+	$presenter_wporg_username = filter_input( INPUT_POST, 'presenter-wporg-username' );
+	$presenter_usernames      = array_map( 'trim', explode( ',', $presenter_wporg_username ) );
+	delete_post_meta( $post_id, 'presenter_wporg_username' );
+	if ( is_array( $presenter_usernames ) ) {
+		foreach ( $presenter_usernames as $username ) {
+			add_post_meta( $post_id, 'presenter_wporg_username', $username );
+		}
+	}
+
+	$other_contributor_wporg_username = filter_input( INPUT_POST, 'other-contributor-wporg-username' );
+	$other_contributor_usernames      = array_map( 'trim', explode( ',', $other_contributor_wporg_username ) );
+	delete_post_meta( $post_id, 'other_contributor_wporg_username' );
+	if ( is_array( $other_contributor_usernames ) ) {
+		foreach ( $other_contributor_usernames as $username ) {
+			add_post_meta( $post_id, 'other_contributor_wporg_username', $username );
+		}
+	}
+
+	// This language meta field is rendered in the editor sidebar using a PluginDocumentSettingPanel block,
+	// which won't save the field on publish if it has the default value.
+	// Our custom query for locale prioritized lessons (see locale.php `wporg_archive_query_prioritize_locale`)
 	// depends on it being set, so we force it to be updated after saving:
 	$language         = get_post_meta( $post_id, 'language', true );
 	$language_default = 'en_US';
