e0bc302e9cddc7f30a98903eb56e89718df2f4c5
ren <18050944+renintw@users.noreply.github.com>
2024-12-05 21:02:11 +0900

    Show content in collapsible container

--- wp-content/themes/pub/wporg-learn-2024/src/lesson-facilitator-notes/index.js
+++ wp-content/themes/pub/wporg-learn-2024/src/lesson-facilitator-notes/index.js
@@ -4,7 +4,7 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { Button, ComboboxControl } from '@wordpress/components';
-import { useState } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 import { debounce } from 'lodash';
@@ -19,6 +19,23 @@ registerBlockType( metadata.name, {
 	edit: function Edit( { attributes, setAttributes } ) {
 		const { lessonPlanId, lessonPlanContent } = attributes;
 		const [ searchResults, setSearchResults ] = useState( [] );
+		const [ isExpanded, setIsExpanded ] = useState( false );
+
+		useEffect( () => {
+			// Fetch the initial lesson plan options if lessonPlanId is set
+			if ( lessonPlanId ) {
+				apiFetch( {
+					path: `/wp/v2/lesson-plan/${ lessonPlanId }`,
+				} ).then( ( plan ) => {
+					setSearchResults( [
+						{
+							value: plan.id,
+							label: plan.title.rendered,
+						},
+					] );
+				} );
+			}
+		}, [] );
 
 		const fetchLessonPlanContent = ( id ) => {
 			apiFetch( { path: `/wp/v2/lesson-plan/${ id }` } ).then( ( plan ) => {
@@ -58,8 +75,6 @@ registerBlockType( metadata.name, {
 					onFilterValueChange={ ( inputValue ) => {
 						if ( inputValue ) {
 							fetchLessonPlans( inputValue );
-						} else {
-							setSearchResults( [] );
 						}
 					} }
 					onChange={ ( newValue ) => {
@@ -71,11 +86,26 @@ registerBlockType( metadata.name, {
 					placeholder={ __( 'Search for a lesson plan…', 'wporg-learn' ) }
 				/>
 				{ lessonPlanId && (
-					<RichText
-						label={ __( 'Edit Lesson Plan Content', 'wporg-learn' ) }
-						value={ lessonPlanContent }
-						onChange={ ( newContent ) => setAttributes( { lessonPlanContent: newContent } ) }
-					/>
+					<>
+						<Button variant="secondary" onClick={ () => setIsExpanded( ! isExpanded ) }>
+							{ isExpanded
+								? __( 'Collapse Content', 'wporg-learn' )
+								: __( 'Expand Content', 'wporg-learn' ) }
+						</Button>
+						<div
+							style={ {
+								maxHeight: isExpanded ? 'none' : '100px', // Adjust the height as needed
+								overflow: 'hidden',
+								transition: 'max-height 0.3s ease',
+							} }
+						>
+							<RichText
+								label={ __( 'Edit Lesson Plan Content', 'wporg-learn' ) }
+								value={ lessonPlanContent }
+								onChange={ ( newContent ) => setAttributes( { lessonPlanContent: newContent } ) }
+							/>
+						</div>
+					</>
 				) }
 				{ lessonPlanId && (
 					<Button variant="primary" onClick={ saveLessonPlanContent }>
