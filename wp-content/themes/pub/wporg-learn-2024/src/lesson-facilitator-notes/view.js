afb7fadb7a782f8f41b137c7503e98767cea8b6f
ren <18050944+renintw@users.noreply.github.com>
2024-12-10 15:15:03 +0900

    Add mobile view styles for the label

--- wp-content/themes/pub/wporg-learn-2024/src/lesson-facilitator-notes/view.js
+++ wp-content/themes/pub/wporg-learn-2024/src/lesson-facilitator-notes/view.js
@@ -1,8 +1,10 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 	const facilitatorNotes = document.querySelector( '.wporg-learn-lesson-facilitator-notes-label' );
-	const headerInfo = document.querySelector( '.sensei-course-theme__header__info' );
+	const headerInfos = document.querySelectorAll( '.sensei-course-theme-course-progress' );
 
-	if ( facilitatorNotes && headerInfo ) {
-		headerInfo.insertBefore( facilitatorNotes, headerInfo.lastChild );
+	if ( facilitatorNotes && headerInfos.length > 0 ) {
+		headerInfos.forEach( ( headerInfo ) => {
+			headerInfo.insertAdjacentElement( 'afterend', facilitatorNotes.cloneNode( true ) );
+		} );
 	}
 } );
