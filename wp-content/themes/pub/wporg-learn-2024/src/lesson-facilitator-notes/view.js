9ec5620a0a6bf45f14dfb97dac6f7cddf6558098
ren <18050944+renintw@users.noreply.github.com>
2024-12-10 17:04:09 +0900

    Adjust label style for standalone lesson

--- wp-content/themes/pub/wporg-learn-2024/src/lesson-facilitator-notes/view.js
+++ wp-content/themes/pub/wporg-learn-2024/src/lesson-facilitator-notes/view.js
@@ -1,10 +1,15 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 	const facilitatorNotes = document.querySelector( '.wporg-learn-lesson-facilitator-notes-label' );
 	const headerInfos = document.querySelectorAll( '.sensei-course-theme-course-progress' );
+	const headerContent = document.querySelector( '.sensei-course-theme-header-content' );
 
 	if ( facilitatorNotes && headerInfos.length > 0 ) {
 		headerInfos.forEach( ( headerInfo ) => {
 			headerInfo.insertAdjacentElement( 'afterend', facilitatorNotes.cloneNode( true ) );
 		} );
+	} else if ( facilitatorNotes && headerContent ) {
+		headerContent.firstElementChild.insertAdjacentElement( 'afterend', facilitatorNotes.cloneNode( true ) );
 	}
+
+	facilitatorNotes.remove();
 } );
