e2cc6c4dacafa2e2bcfc045c757808fbf53d29d4
Dion Hulse <dion@wordpress.org>
2025-05-27 15:47:44 +1000

    Avoid PHP Fatal when duration fields are left blank. (#3158)
    If the duration fields are left blank, under PHP8 a fatal will be thrown:
    
    > E_ERROR: Uncaught TypeError: Unsupported operand types: string * int

--- wp-content/plugins/wporg-learn/inc/post-meta.php
+++ wp-content/plugins/wporg-learn/inc/post-meta.php
@@ -664,6 +664,7 @@ function save_workshop_meta_fields( $post_id ) {
 
 	$duration = filter_input( INPUT_POST, 'duration', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY );
 	if ( isset( $duration['h'], $duration['m'], $duration['s'] ) ) {
+		$duration = array_map( 'absint', $duration );
 		$duration = $duration['h'] * HOUR_IN_SECONDS + $duration['m'] * MINUTE_IN_SECONDS + $duration['s'];
 		update_post_meta( $post_id, 'duration', $duration );
 	}
